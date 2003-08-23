e78fe4f79b0f011375718b6860d87e7fae30d436
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-08-23 09:34:40 +0000

    A patch from Megan Fox that lets multiple workspaces get generated.

--- trunk/nebula2/code/nebula2/src/update.tcl
+++ trunk/nebula2/code/nebula2/src/update.tcl
@@ -31,14 +31,34 @@ if { [catch { exec uuidgen }] } {
 	puts "uuidgen.exe not found skipping Visual Studio Solutions."
 } else {
 	puts "uuidgen.exe found"
+	# Do the default two first
 	gen_solution nebulalib 0
 	gen_solution tools 0	
+
+	# Then, Check the rest of the data for type "workspace" - and if they're a workspace, call gen_solution on them
+	global tar
+	global num_tars
+	for {set i 0} {$i < $num_tars} {incr i} {
+		if {"workspace" == $tar($i,type) && "nebulalib" != $tar($i,name) && "tools" != $tar($i,name)} {
+			gen_solution $tar($i,name) 0
+		}
+	}
 }
 gen_makefile
 # These must come after the solution files
-# as we add some defauly libs to libs_win32
+# as we add some default libs to libs_win32
+# First add the default workspaces
 gen_workspace nebulalib 0
 gen_workspace tools 0
+
+# Then, Check the rest of the data for type "workspace" - and if they're a workspace, call gen_workspace on them
+global tar
+global num_tars
+for {set i 0} {$i < $num_tars} {incr i} {
+	if {"workspace" == $tar($i,type) && "nebulalib" != $tar($i,name) && "tools" != $tar($i,name)} {
+		gen_workspace $tar($i,name) 0
+	}
+}
 
 puts "done."
 
