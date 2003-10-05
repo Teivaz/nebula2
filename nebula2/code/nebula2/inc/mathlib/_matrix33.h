3669468878d84864c9945f479f85332cb986362e
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-10-05 19:26:43 +0000

    This patch adds some optimizations originally suggested by Michaele Norel to the matrix lookat and billboard functions and the quaternion set_from_axes function. The patch looks good to me, but I haven't time to test it right now, if someone could double check these against the old functions that would be very helpful. The same fixes apply to Nebula1 too, perhaps someone could whip up a patch for that too.

--- nebula2/code/nebula2/inc/mathlib/_matrix33.h
+++ nebula2/code/nebula2/inc/mathlib/_matrix33.h
@@ -333,11 +333,9 @@ _matrix33::lookat(const _vector3& from, const _vector3& to, const _vector3& up)
 {
     _vector3 z(from - to);
     z.norm();
-    _vector3 y(up);
-    _vector3 x(y * z);   // x = y cross z
-    y = z * x;          // y = z cross x
+    _vector3 x(up * z);   // x = y cross z
     x.norm();
-    y.norm();
+    _vector3 y = z * x;   // y = z cross x
 
     M11=x.x;  M12=x.y;  M13=x.z;
     M21=y.x;  M22=y.y;  M23=y.z;
@@ -354,11 +352,9 @@ _matrix33::billboard(const _vector3& from, const _vector3& to, const _vector3& u
     _vector3 z(from - to);
     z.norm();
     _vector3 y(up);
+    y.norm();
     _vector3 x(y * z);
     z = x * y;
-    x.norm();
-    y.norm();
-    z.norm();
 
     M11=x.x;  M12=x.y;  M13=x.z;
     M21=y.x;  M22=y.y;  M23=y.z;
