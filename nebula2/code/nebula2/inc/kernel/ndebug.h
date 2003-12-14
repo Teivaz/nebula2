5bd22eef40e0802d91d396e6e488a52061c64bbc
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-14 09:40:48 +0000

    Small patch from Rafael adding n_verify().

--- nebula2/code/nebula2/inc/kernel/ndebug.h
+++ nebula2/code/nebula2/inc/kernel/ndebug.h
@@ -5,13 +5,16 @@
     Nebula debug macros.
   
     n_assert()  - the vanilla assert() Macro
+    n_verify()  - like assert() except that the statement in parens is simply evaluated, unchecked, if __NEBULA_NO_ASSERT is next
 */
 #include <stdlib.h>
 
 #ifdef __NEBULA_NO_ASSERT__
 #define n_assert(exp)
+#define n_verify(exp) (exp)
 #else
 #define n_assert(exp) { if (!(exp)) n_barf(#exp,__FILE__,__LINE__); }
+#define n_verify(exp) { if (!(exp)) n_barf(#exp,__FILE__,__LINE__); }
 #endif
 //------------------------------------------------------------------------------
 #endif
