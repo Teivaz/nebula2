9c755748e8f613a2ace0a303b266c2aca128f934
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-16 09:33:10 +0000

    Missing copyright/license notices.

--- nebula2/code/contrib/njava/inc/java/njavautils.h
+++ nebula2/code/contrib/njava/inc/java/njavautils.h
@@ -1,6 +1,13 @@
 #ifndef N_JAVAUTILS_H
 #define N_JAVAUTILS_H
+//------------------------------------------------------------------------------
+/**
+    @brief Some utility functions for data conversion for nJava.
 
+    This file is licensed under the terms of the Nebula License.
+    (C) 2003 Bruce Mitchener, Jr.
+*/
+//------------------------------------------------------------------------------
 #include <jni.h>
 
 class nArg;
