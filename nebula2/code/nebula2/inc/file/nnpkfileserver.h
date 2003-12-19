6f1a8df050023225caf63965a87b25eb344fb361
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-19 13:50:05 +0000

    Merge Changes:  * removed N_PUBLIC  * removed external include guards

--- nebula2/code/nebula2/inc/file/nnpkfileserver.h
+++ nebula2/code/nebula2/inc/file/nnpkfileserver.h
@@ -51,14 +51,7 @@
 
     (C) 2002 RadonLabs GmbH
 */
-
-#ifndef N_FILESERVER2_H
 #include "kernel/nfileserver2.h"
-#endif
-
-#undef N_DEFINES
-#define N_DEFINES nNpkFileServer
-#include "kernel/ndefdllclass.h"
 
 //------------------------------------------------------------------------------
 class nNpkTocEntry;
