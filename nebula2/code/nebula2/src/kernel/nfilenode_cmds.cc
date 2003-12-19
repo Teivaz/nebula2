8fdaeb582a5a28980aafafdc5632e9bcdc050374
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-19 22:50:28 +0000

    New nFileNode from Radon Labs, for script languages that don't have file system access built in (e.g. MicroTcl).

---
+++ nebula2/code/nebula2/src/kernel/nfilenode_cmds.cc
@@ -0,0 +1,120 @@
+//------------------------------------------------------------------------------
+//  nfilenode_cmds.cc
+//  (C) 2003 RadonLabs GmbH
+//------------------------------------------------------------------------------
+#include "kernel/nfilenode.h"
+
+static void n_open(void* slf, nCmd* cmd);
+static void n_close(void* slf, nCmd* cmd);
+static void n_isopen(void* slf, nCmd* cmd);
+static void n_puts(void* slf, nCmd* cmd);
+
+//------------------------------------------------------------------------------
+/**
+    @scriptclass
+    nfileserver2
+    
+    @superclass
+    nroot
+
+    @classinfo
+    New style fileserver.
+*/
+void
+n_initcmds(nClass* cl)
+{
+    cl->BeginCmds();
+    cl->AddCmd("b_open_ss",  'OPEN', n_open);
+    cl->AddCmd("v_close_v",  'CLOS', n_close);
+    cl->AddCmd("b_isopen_v", 'ISOP', n_isopen);
+    cl->AddCmd("b_puts_s",   'PUTS', n_puts);
+    cl->EndCmds();
+}
+
+//------------------------------------------------------------------------------
+/**
+    @cmd
+    open
+    
+    @input
+    s(Filename), s(AccessMode)
+    
+    @output
+    v
+    
+    @info
+    Open a file. Filename can be a Nebula filename using path assigns. 
+    AccessMode is a string containing an "r" for read-mode, and/or 
+    "w" for write-mode.
+*/
+static void
+n_open(void* slf, nCmd* cmd)
+{
+    nFileNode* self = (nFileNode*) slf;
+    const char* s0 = cmd->In()->GetS();
+    const char* s1 = cmd->In()->GetS();
+    cmd->Out()->SetB(self->Open(s0, s1));
+}
+
+//------------------------------------------------------------------------------
+/**
+    @cmd
+    close
+    
+    @input
+    v
+    
+    @output
+    v
+    
+    @info
+    Closes a file.
+*/
+static void
+n_close(void* slf, nCmd* cmd)
+{
+    nFileNode* self = (nFileNode*) slf;
+    self->Close();
+}
+
+//------------------------------------------------------------------------------
+/**
+    @cmd
+    isopen
+    
+    @input
+    v
+    
+    @output
+    b(IsOpen)
+    
+    @info
+    Return "true" if file is open.
+*/
+static void
+n_isopen(void* slf, nCmd* cmd)
+{
+    nFileNode* self = (nFileNode*) slf;
+    cmd->Out()->SetB(self->IsOpen());
+}
+
+//------------------------------------------------------------------------------
+/**
+    @cmd
+    puts
+    
+    @input
+    s(String)
+    
+    @output
+    b(Success)
+    
+    @info
+    Put a string into the file.
+*/
+static void
+n_puts(void* slf, nCmd* cmd)
+{
+    nFileNode* self = (nFileNode*) slf;
+    cmd->Out()->SetB(self->PutS(cmd->In()->GetS()));
+}
