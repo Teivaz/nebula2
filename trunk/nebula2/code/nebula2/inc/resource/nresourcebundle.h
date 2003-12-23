3b43f07ac8e411c7a1c266451e3e965609512e7e
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-23 03:46:38 +0000

    New nResourceBundle class from RL. A resource bundle is a bunch of resources stored in a single binary file on disk. Bundles can be created using the new resource compiler tool (which will be added to CVS soon).

---
+++ trunk/nebula2/code/nebula2/inc/resource/nresourcebundle.h
@@ -0,0 +1,49 @@
+#ifndef N_RESOURCEBUNDLE_H
+#define N_RESOURCEBUDNLE_H
+//------------------------------------------------------------------------------
+/**
+    @class nResourceBundle
+
+    Load a resource bundle from disc and register all contained resources.
+    
+    (C) 2003 RadonLabs GmbH
+*/
+#include "resource/nresource.h"
+#include "kernel/nautoref.h"
+
+class nGfxServer2;
+class nAnimationServer;
+
+//------------------------------------------------------------------------------
+class nResourceBundle : public nResource
+{
+public:
+    /// constructor
+    nResourceBundle();
+    /// destructor
+    virtual ~nResourceBundle();
+
+    static nKernelServer* kernelServer;
+
+protected:
+    /// load the resource (sets the valid flag)
+    virtual bool LoadResource();
+    /// unload the resource (clears the valid flag)
+    virtual void UnloadResource();
+
+private:
+    /// load a single resource from the resource bundle file
+    bool LoadResource(int type, const char* resId, int dataOffset, int dataSize, int flags, nFile* dataFile);
+    /// load a mesh resource from the resource bundle file
+    bool LoadMesh(const char* resId,  int dataOffset, int dataSize, int flags, nFile* dataFile);
+    /// load an animation resource from the resource bundle file
+    bool LoadAnimation(const char* resId, int dataOffset, int dataSize, int flags, nFile* dataFile);
+    /// load a texture resource from the resource bundle file
+    bool LoadTexture(const char* resId, int dataOffset, int dataSize, int flags, nFile* dataFile);
+
+    nAutoRef<nGfxServer2> refGfxServer;
+    nAutoRef<nAnimationServer> refAnimServer;
+    nArray< nRef<nResource> > bundleResources;  // resource objects associated with this bundle
+};
+//------------------------------------------------------------------------------
+#endif
