d275d8b59b9975ea1a3615b585e3b0d641b3380e
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-23 04:09:37 +0000

    - Removed external include guards. - nResource::Type name have been renamed. - nResources can now be loaded asynchronously, a pretty good overview of this feature is provided in the documentation (in nresource.h).

--- trunk/nebula2/code/nebula2/src/resource/nresource_main.cc
+++ trunk/nebula2/code/nebula2/src/resource/nresource_main.cc
@@ -4,6 +4,7 @@
 //  (C) 2001 RadonLabs GmbH
 //------------------------------------------------------------------------------
 #include "resource/nresource.h"
+#include "resource/nresourceserver.h"
 #include "kernel/nkernelserver.h"
 
 nNebulaClass(nResource, "nroot");
@@ -12,9 +13,12 @@ nNebulaClass(nResource, "nroot");
 /**
 */
 nResource::nResource() :
+    refResourceServer("/sys/servers/resource"),
     refFileServer("/sys/servers/file2"),
-    type(NONE),
-    valid(false)
+    type(InvalidResourceType),
+    asyncEnabled(false),
+    isValid(false),
+    jobNode(this)
 {
     // empty
 }
@@ -25,56 +29,13 @@ nResource::nResource() :
 nResource::~nResource()
 {
     n_assert(!this->IsValid());
-}
-
-//------------------------------------------------------------------------------
-/**
-    Set the resource type.
-
-    @param  t   resource type
-*/
-void
-nResource::SetType(Type t)
-{
-    this->type = t;
-}
-
-//------------------------------------------------------------------------------
-/**
-    Get the resource type.
-
-    @return     resource type
-*/
-nResource::Type
-nResource::GetType() const
-{
-    return this->type;
-}
-
-//------------------------------------------------------------------------------
-/**
-    Set the absolute path(!) to the resource file. The path can contain
-    Nebula-Assigns.
-
-    @param  path    absolute path to the resource file
-*/
-void
-nResource::SetFilename(const char* path)
-{
-    n_assert(path);
-    this->filename = path;
-}
-
-//------------------------------------------------------------------------------
-/**
-    Get the absolute path to the resource file.
-
-    @return     path, or 0 if not initialized yet
-*/
-const char*
-nResource::GetFilename() const
-{
-    return this->filename.IsEmpty() ? 0 : this->filename.Get();
+    
+    // if we are pending for an async load, we must remove ourselves 
+    // from the loader job list.
+    if (this->refResourceServer.isvalid())
+    {
+        this->refResourceServer->RemLoaderJob(this);
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -103,37 +64,119 @@ nResource::GetResourceLoader()
 
 //------------------------------------------------------------------------------
 /**
-    Return whether the resource is valid (loaded) or not (unloaded).
-
-    @return     valid flag
+    Subclasses must override this method to indicate to the nResource class
+    whether they support asynchronous resource loading. Default is false.
 */
 bool
-nResource::IsValid() const
-{
-    return this->valid;
-}
-
-//------------------------------------------------------------------------------
-/**
-    Load the resource from file, and set the valid flag if successful.
-    This method should be modified by subclasses.
-
-    @return     true if resource data successfully loaded
-*/
-bool
-nResource::Load()
+nResource::CanLoadAsync() const
 {
     return false;
 }
 
 //------------------------------------------------------------------------------
 /**
-    Unload the resource data, freeing runtime resources. This method
-    should be modified by subclasses.
+    Request to load a resource in synchronous or asynchronous mode. Will
+    care about multithreading issues and invoke the LoadResource() method
+    which should be overriden by subclasses to implement the actual loading.
+    Subclasses must indicate with the CanLoadAsync() method whether they
+    support asynchronous loading or not.
+
+    NOTE: in asynchronous mode, the method will return true although the
+    resource data is not available yet. Use the IsValid() method to 
+    check when the resource data is available.
+
+    @return     true if all ok, 
+*/
+bool
+nResource::Load()
+{
+    // if we are already valid, do nothing
+    if (this->IsValid())
+    {
+        return true;
+    }
+
+    #ifndef __NEBULA_NO_THREADS__
+    if (this->GetAsyncEnabled() && this->CanLoadAsync())
+    {
+        // if a load request is already pending, do nothing
+        if (this->IsPending())
+        {
+            return true;
+        }
+        else
+        {
+            // otherwise, add to resource server's loader jobs
+            this->refResourceServer->AddLoaderJob(this);
+            return true;
+        }
+    }
+    else
+    #endif
+    {
+        // the synchronous case is simply
+        return this->LoadResource();
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+    Loads the resource from within an open file.
+    This method should be overriden by subclasses.
+
+    @return     true if resource data successfully loaded
+*/
+bool
+nResource::Load(nFile* file, int offset, int length)
+{
+    n_error("nResource::Load(file, offset, length) not implemented yet!");
+    return false;
+}
+
+//------------------------------------------------------------------------------
+/**
+    Unload the resource data, freeing runtime resources. This method will call
+    the protected virtual UnloadResources() method which should be overriden
+    by subclasses.
+    This method works in sync and async mode and care about the multithreading
+    issues before and after calling LoadResources();
 */
 void
 nResource::Unload()
 {
+    // remove from loader list, if pending for async load
+    if (this->refResourceServer.isvalid())
+    {
+        this->refResourceServer->RemLoaderJob(this);
+    }
+    if (this->IsValid())
+    {
+        this->UnloadResource();
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+    Subclasses must override this method and implement the actual
+    resource loading code here. The method may get called from a thread
+    (if the class returns true in the CanLoadAsync() method and the
+    resource object works in async mode).
+*/
+bool
+nResource::LoadResource()
+{
+    return true;
+}
+
+//------------------------------------------------------------------------------
+/**
+    Subclasses must override this method and implement the resource unloading.
+    This method will always run in the main thread.
+*/
+void
+nResource::UnloadResource()
+{
     // empty
 }
+
 
