26c4d109aa8f90a29f1681ae29308b08ed0ac257
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-24 11:39:57 +0000

    New font resource and d3d subclass. Note: I've changed this from Radon Labs version to use ID3DXFont9.

---
+++ nebula2/code/nebula2/inc/gfx2/nd3d9font.h
@@ -0,0 +1,53 @@
+#ifndef N_D3D9FONT_H
+#define N_D3D9FONT_H
+//------------------------------------------------------------------------------
+/**
+    @class nD3D9Font
+
+    A D3D9 specific subclass of nFont2.
+    
+    (C) 2003 RadonLabs GmbH
+*/
+#include <d3dx.h>
+#include "gfx2/nfont2.h"
+#include "kernel/nautoref.h"
+
+class nD3D9Server;
+
+//------------------------------------------------------------------------------
+class nD3D9Font : public nFont2
+{
+public:
+    /// constructor
+    nD3D9Font();
+    /// destructor
+    virtual ~nD3D9Font();
+    /// access to the internal CD3DFont object.
+    ID3DXFont9* GetD3DFont() const;
+
+    static nKernelServer* kernelServer;
+
+protected:
+    /// load resource
+    virtual bool LoadResource();
+    /// unload resource
+    virtual void UnloadResource();
+
+private:
+    nAutoRef<nD3D9Server> refD3D9Server;
+    ID3DXFont9* d3dFont;
+};
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+CD3DFont9*
+nD3D9Font::GetD3DFont() const
+{
+    n_assert(this->d3dFont);
+    return this->d3dFont;
+}
+
+//------------------------------------------------------------------------------
+#endif
---
+++ nebula2/code/nebula2/inc/gfx2/nfont2.h
@@ -0,0 +1,34 @@
+#ifndef N_FONT2_H
+#define N_FONT2_H
+//------------------------------------------------------------------------------
+/**
+    @class nFont2
+
+    A font resource class.
+    
+    (C) 2003 RadonLabs GmbH
+*/
+#include "resource/nresource.h"
+#include "gfx2/nfontdesc.h"
+
+//------------------------------------------------------------------------------
+class nFont2 : public nResource
+{
+public:
+    /// constructor
+    nFont2();
+    /// destructor
+    virtual ~nFont2();
+    /// set font description
+    void SetFontDesc(const nFontDesc& desc);
+    /// get font description
+    const nFontDesc& GetFontDesc() const;
+
+    static nKernelServer* kernelServer;
+
+protected:
+    nFontDesc fontDesc;
+};
+
+//------------------------------------------------------------------------------
+#endif
