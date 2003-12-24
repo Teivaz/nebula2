9d51bf04d011f38bf545db965d8055d8bdccc944
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-24 02:09:43 +0000

    New nLightNode and nSpotLightNode from RL, these replace nVolumeLightNode (which will be removed from CVS soon).

---
+++ trunk/nebula2/code/nebula2/inc/scene/nspotlightnode.h
@@ -0,0 +1,41 @@
+#ifndef N_SPOTLIGHTNODE_H
+#define N_SPOTLIGHTNODE_H
+//------------------------------------------------------------------------------
+/**
+    @class nSpotLightNode
+
+    A light node with a projection matrix describing a pyramidal light cone.
+
+    (C) 2003 RadonLabs GmbH
+*/
+#include "scene/nlightnode.h"
+#include "gfx2/ncamera2.h"
+
+//------------------------------------------------------------------------------
+class nSpotLightNode : public nLightNode
+{
+public:
+    /// constructor
+    nSpotLightNode();
+    /// destructor
+    virtual ~nSpotLightNode();
+    /// save object to persistent stream
+    virtual bool SaveCmds(nPersistServer* ps);
+    /// set the volume projection matrix in the current shader
+    virtual bool RenderLight(nSceneServer* sceneServer, nRenderContext* renderContext, const matrix44& lightTransform);
+    /// set perspective parameters
+    void SetPerspective(float aov, float aspect, float nearp, float farp);
+    /// set orthogonal parameters
+    void SetOrthogonal(float w, float h, float nearp, float farp);
+    /// set the cone definition directly
+    void SetCamera(const nCamera2& c);
+    /// get the cone definition
+    const nCamera2& GetCamera() const;
+
+    static nKernelServer* kernelServer;
+
+private:
+    nCamera2 camera;
+};
+//------------------------------------------------------------------------------
+#endif
