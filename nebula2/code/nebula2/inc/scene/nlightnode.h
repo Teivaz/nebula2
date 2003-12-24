19609cfca7fc65db1bbd9bee872a5110ca09aa3f
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-24 02:09:43 +0000

    New nLightNode and nSpotLightNode from RL, these replace nVolumeLightNode (which will be removed from CVS soon).

---
+++ nebula2/code/nebula2/inc/scene/nlightnode.h
@@ -0,0 +1,32 @@
+#ifndef N_LIGHTNODE_H
+#define N_LIGHTNODE_H
+//------------------------------------------------------------------------------
+/**
+    @class nLightNode
+ 
+    Base class of scene nodes that provide lighting information.
+
+    (C) 2003 RadonLabs GmbH
+*/
+#include "scene/nabstractshadernode.h"
+
+//------------------------------------------------------------------------------
+class nLightNode : public nAbstractShaderNode
+{
+public:
+    /// constructor
+    nLightNode();
+    /// destructor
+    virtual ~nLightNode();
+    /// return true if node provides lighting information
+    virtual bool HasLight() const;
+    /// set the user defined shader parameters
+    virtual bool RenderLight(nSceneServer* sceneServer, nRenderContext* renderContext, const matrix44& lightTransform);
+
+    static nKernelServer* kernelServer;
+};
+//------------------------------------------------------------------------------
+#endif
+
+
+
---
+++ nebula2/code/nebula2/src/scene/nlightnode_main.cc
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------
+//  nlightnode_main.cc
+//  (C) 2003 RadonLabs GmbH
+//------------------------------------------------------------------------------
+#include "scene/nlightnode.h"
+#include "gfx2/nshader2.h"
+#include "gfx2/ngfxserver2.h"
+
+nNebulaClass(nLightNode, "nabstractshadernode");
+
+//------------------------------------------------------------------------------
+/**
+*/
+nLightNode::nLightNode()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+nLightNode::~nLightNode()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+nLightNode::HasLight() const
+{
+    return true;
+}
+
+//------------------------------------------------------------------------------
+/**
+    "Render" the light into the current shader.
+
+    @param  sceneServer         pointer to the scene server object rendering this node
+    @param  renderContext       pointer to the render context for this node
+    @param  lightTransform      this node's model matrix
+*/
+bool
+nLightNode::RenderLight(nSceneServer* sceneServer, nRenderContext* renderContext, const matrix44& lightTransform)
+{
+    n_assert(sceneServer);
+    n_assert(renderContext);
+
+    nGfxServer2* gfxServer = this->refGfxServer.get();
+
+    // invoke shader animators
+    this->InvokeShaderAnimators(renderContext);
+
+    // see if any resources need to be reloaded
+    if (!this->AreResourcesValid())
+    {
+        this->LoadResources();
+    }
+    
+    // get the current shader object from the gfx server
+    nShader2* shader = this->refGfxServer->GetShader();
+    n_assert(shader);
+
+    // apply int, float and vector variables to the shader
+    shader->SetParams(this->shaderParams);
+
+    return true;
+}
+
