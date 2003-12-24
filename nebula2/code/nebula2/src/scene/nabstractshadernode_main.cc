06a6fc670b94d4bcb3df28be3c39378edcc61111
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-24 05:37:30 +0000

    - Removed external header guards. - Modified to work with the new shader parameter mechanism instead of nVariables, Get*S()/Set*S() methods removed in the process.

--- nebula2/code/nebula2/src/scene/nabstractshadernode_main.cc
+++ nebula2/code/nebula2/src/scene/nabstractshadernode_main.cc
@@ -1,4 +1,3 @@
-#define N_IMPLEMENTS nAbstractShaderNode
 //------------------------------------------------------------------------------
 //  nabstractshadernode_main.cc
 //  (C) 2003 RadonLabs GmbH
@@ -11,9 +10,7 @@ nNebulaScriptClass(nAbstractShaderNode, "ntransformnode");
 //------------------------------------------------------------------------------
 /**
 */
-nAbstractShaderNode::nAbstractShaderNode() :
-    refGfxServer("/sys/servers/gfx"),
-    refVariableServer("/sys/servers/variable")
+nAbstractShaderNode::nAbstractShaderNode()
 {
     // empty
 }
@@ -51,18 +48,23 @@ nAbstractShaderNode::LoadTexture(int index)
     TexNode& texNode = this->texNodeArray[index];
     if ((!texNode.refTexture.isvalid()) && (!texNode.texName.IsEmpty()))
     {
-        nTexture2* tex = this->refGfxServer->NewTexture(texNode.texName.Get());
-        n_assert(tex);
-        if (!tex->IsValid())
+        // load only if the texture is used in the shader
+        if (this->IsTextureUsed(texNode.shaderParameter))
         {
-            tex->SetFilename(texNode.texName.Get());
-            if (!tex->Load())
+            nTexture2* tex = this->refGfxServer->NewTexture(texNode.texName.Get());
+            n_assert(tex);
+            if (!tex->IsValid())
             {
-                n_printf("nMaterialNode::Error loading texture '%s'\n", texNode.texName.Get());
-                return false;
+                tex->SetFilename(texNode.texName.Get());
+                if (!tex->Load())
+                {
+                        n_printf("nAbstractShaderNode: Error loading texture '%s'\n", texNode.texName.Get());
+                    return false;
+                }
             }
+            texNode.refTexture = tex;
+            this->shaderParams.SetTexture(texNode.shaderParameter, tex);
         }
-        texNode.refTexture = tex;
     }
     return true;
 }
@@ -89,33 +91,6 @@ nAbstractShaderNode::LoadResources()
 
 //------------------------------------------------------------------------------
 /**
-    Return false if invalid resources exist.
-*/
-bool
-nAbstractShaderNode::AreResourcesValid() const
-{
-    if (nSceneNode::AreResourcesValid())
-    {
-        int i;
-        int num = this->texNodeArray.Size();
-        for (i = 0; i < num; i++)
-        {
-            if (!this->texNodeArray[i].refTexture.isvalid())
-            {
-                return false;
-            }
-        }
-        // fallthrough: all textures valid
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-//------------------------------------------------------------------------------
-/**
     Unload the resources if refcount has reached zero.
 */
 void
@@ -134,17 +109,23 @@ nAbstractShaderNode::UnloadResources()
 /**
 */
 void
-nAbstractShaderNode::SetTexture(nVariable::Handle varHandle, const char* texName)
+nAbstractShaderNode::SetTexture(nShader2::Parameter param, const char* texName)
 {
-    n_assert(nVariable::INVALID_HANDLE != varHandle);
     n_assert(texName);
+
+    // silently ignore invalid parameters
+    if (nShader2::InvalidParameter == param)
+    {
+        n_printf("WARNING: invalid shader parameter in object '%s'\n", this->GetName());
+        return;
+    }
 
     // see if texture variable already exists
     int i;
     int num = this->texNodeArray.Size();
     for (i = 0; i < num; i++)
     {
-        if (this->texNodeArray[i].varHandle == varHandle)
+        if (this->texNodeArray[i].shaderParameter == param)
         {
             break;
         }
@@ -152,8 +133,8 @@ nAbstractShaderNode::SetTexture(nVariable::Handle varHandle, const char* texName
     if (i == num)
     {
         // add new texnode to array
-        TexNode newTexNode(varHandle, texName);
-        this->texNodeArray.PushBack(newTexNode);
+        TexNode newTexNode(param, texName);
+        this->texNodeArray.Append(newTexNode);
     }
     else
     {
@@ -167,15 +148,13 @@ nAbstractShaderNode::SetTexture(nVariable::Handle varHandle, const char* texName
 /**
 */
 const char*
-nAbstractShaderNode::GetTexture(nVariable::Handle varHandle) const
+nAbstractShaderNode::GetTexture(nShader2::Parameter param) const
 {
-    n_assert(nVariable::INVALID_HANDLE != varHandle);
-
     int i;
     int num = this->texNodeArray.Size();
     for (i = 0; i < num; i++)
     {
-        if (this->texNodeArray[i].varHandle == varHandle)
+        if (this->texNodeArray[i].shaderParameter == param)
         {
             return texNodeArray[i].texName.Get();
         }
@@ -183,101 +162,4 @@ nAbstractShaderNode::GetTexture(nVariable::Handle varHandle) const
     // fallthrough: invalid variable name
     return 0;
 }
-
-//------------------------------------------------------------------------------
-/**
-*/
-void
-nAbstractShaderNode::SetInt(nVariable::Handle varHandle, int val)
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    if (var)
-    {
-        var->SetInt(val);
-    }
-    else
-    {
-        this->varContext.AddVariable(nVariable(varHandle, val));
-    }
-}
-
-//------------------------------------------------------------------------------
-/**
-*/
-int
-nAbstractShaderNode::GetInt(nVariable::Handle varHandle) const
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    n_assert(var);
-    return var->GetInt();
-}
-
-//------------------------------------------------------------------------------
-/**
-*/
-void
-nAbstractShaderNode::SetFloat(nVariable::Handle varHandle, float val)
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    if (var)
-    {
-        var->SetFloat(val);
-    }
-    else
-    {
-        nVariable newVar(varHandle, val);
-        this->varContext.AddVariable(nVariable(varHandle, val));
-    }
-}
-
-//------------------------------------------------------------------------------
-/**
-*/
-float
-nAbstractShaderNode::GetFloat(nVariable::Handle varHandle) const
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    n_assert(var);
-    return var->GetFloat();
-}
-
-//------------------------------------------------------------------------------
-/**
-*/
-void
-nAbstractShaderNode::SetVector(nVariable::Handle varHandle, const vector4& val)
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    float4 float4Val = 
-    {
-        val.x, val.y, val.z, val.w
-    };
-    if (var)
-    {
-        var->SetVector(float4Val);
-    }
-    else
-    {
-        this->varContext.AddVariable(nVariable(varHandle, float4Val));
-    }
-}
-
-//------------------------------------------------------------------------------
-/**
-*/
-vector4
-nAbstractShaderNode::GetVector(nVariable::Handle varHandle) const
-{
-    n_assert(varHandle != nVariable::INVALID_HANDLE);
-    nVariable* var = this->varContext.GetVariable(varHandle);
-    n_assert(var);
-    const float4& float4Val = var->GetVector();
-    return vector4(float4Val[0], float4Val[1], float4Val[2], float4Val[3]);
-}
-
 
