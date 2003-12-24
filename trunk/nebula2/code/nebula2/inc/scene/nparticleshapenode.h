031b3960647faaece67aad0f21d7aeda29643945
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2003-12-24 01:51:42 +0000

    New particle system from Radon Labs.

---
+++ trunk/nebula2/code/nebula2/inc/scene/nparticleshapenode.h
@@ -0,0 +1,262 @@
+#ifndef N_PARTICLESHAPENODE_H
+#define N_PARTICLESHAPENODE_H
+//------------------------------------------------------------------------------
+/**
+    @class nParticleShapeNode
+
+    A shape node representing a particle system.
+
+    (C) 2003 RadonLabs GmbH
+*/
+#include "scene/nshapenode.h"
+#include "gfx2/ndynamicmesh.h"
+#include "particle/nparticleemitter.h"
+#include "particle/nparticleserver.h"
+
+class nRenderContext;
+//------------------------------------------------------------------------------
+class nParticleShapeNode : public nShapeNode
+{
+public:
+    /// constructor
+    nParticleShapeNode();
+    /// destructor
+    virtual ~nParticleShapeNode();
+    /// object persistency
+    virtual bool SaveCmds(nPersistServer *ps);
+    /// called by app when new render context has been created for this object
+    virtual void RenderContextCreated(nRenderContext* renderContext);
+    /// called by nSceneServer when object is attached to scene
+    virtual void Attach(nSceneServer* sceneServer, nRenderContext* renderContext);
+    /// render geometry
+    virtual bool RenderGeometry(nSceneServer* sceneServer, nRenderContext* renderContext);
+    /// get the mesh usage flags required by this shape node
+    virtual int GetMeshUsage() const;
+    /// return true if node provides transformion
+    virtual bool HasTransform() const;
+    /// update transform and render into scene server
+    virtual bool RenderTransform(nSceneServer* sceneServer, nRenderContext* renderContext, const matrix44& parentMatrix);
+
+    /// set the end time
+    void SetEmissionDuration(nTime time);
+    /// set if loop emitter or not
+    void SetLoop(bool b);
+    /// set the activity distance
+    void SetActivityDistance(float f);
+    /// set the inner cone
+    void SetSpreadAngle(float f);
+    /// set the maximum birth delay
+    void SetBirthDelay(float f);
+    /// set the maximum start rotation angle
+    void SetStartRotation(float f);
+
+    /// get the emission duration
+    nTime GetEmissionDuration() const;
+    /// is loop emitter ?
+    bool GetLoop() const;
+    /// get the activity distance
+    float GetActivityDistance() const;
+    /// get the spread angle
+    float GetSpreadAngle() const;
+    /// get the maximum birth delay
+    float GetBirthDelay() const;
+    /// get the maximum start rotation angle
+    float GetStartRotation() const;
+
+    /// set one of the envelope curves (not the color)
+    void SetCurve(nParticleEmitter::CurveType curveType, const nEnvelopeCurve& curve);
+    /// get one of the envelope curves
+    const nEnvelopeCurve& GetCurve(nParticleEmitter::CurveType curveType) const;
+
+    /// set the particle rgb curve
+    void SetRGBCurve(const nVector3EnvelopeCurve& curve);
+    /// get the particle rgb curve
+    const nVector3EnvelopeCurve& GetRGBCurve() const;
+
+    static nKernelServer* kernelServer;
+
+protected:
+    nAutoRef<nParticleServer> refParticleServer;
+    nAutoRef<nVariableServer> refVariableServer;
+
+    int emitterVarIndex;    // index of the emitter in the rendercontext
+
+    // copy of emitter settings
+
+    nTime           emissionDuration;       // how long shall be emitted ?
+ 
+    bool            loop;                   // loop emitter ?
+
+    float           activityDistance;       // distance between viewer and emitter on which emitter is active
+    float           spreadAngle;            // angle of emitted particle cone
+    float           birthDelay;             // maximum delay until particle starts to live
+    float           startRotation;          // maximum angle of rotation at birth
+
+    nEnvelopeCurve curves[nParticleEmitter::CurveTypeCount];
+    nVector3EnvelopeCurve rgbCurve;
+};
+
+
+//------------------------------------------------------------------------------
+/**
+    particleshape node does not provide transformation.
+*/
+inline bool
+nParticleShapeNode::HasTransform() const
+{
+    return false;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetEmissionDuration(nTime time)
+{
+    this->emissionDuration = time;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline nTime nParticleShapeNode::GetEmissionDuration() const
+{
+    return this->emissionDuration;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetLoop(bool b)
+{
+    this->loop = b;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline bool nParticleShapeNode::GetLoop() const
+{
+    return this->loop;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetActivityDistance(float f)
+{
+    this->activityDistance = f;
+    bbox3  box;
+    box.begin_extend();
+    box.extend(vector3(-activityDistance, -activityDistance, -activityDistance));
+    box.extend(vector3(activityDistance, activityDistance, activityDistance));
+    this->SetLocalBox(box);
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetSpreadAngle(float f)
+{
+    this->spreadAngle = f;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetBirthDelay(float f)
+{
+    this->birthDelay = f;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void nParticleShapeNode::SetStartRotation(float f)
+{
+    this->startRotation = f;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline float nParticleShapeNode::GetActivityDistance() const
+{
+    return this->activityDistance;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline float nParticleShapeNode::GetSpreadAngle() const
+{
+    return this->spreadAngle;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline float nParticleShapeNode::GetBirthDelay() const
+{
+    return this->birthDelay;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline float nParticleShapeNode::GetStartRotation() const
+{
+    return this->startRotation;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+void
+nParticleShapeNode::SetCurve(nParticleEmitter::CurveType curveType, const nEnvelopeCurve& curve)
+{
+    n_assert(curveType < nParticleEmitter::CurveTypeCount);
+    n_assert(curveType >= 0);
+    this->curves[curveType].SetParameters(curve);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const nEnvelopeCurve&
+nParticleShapeNode::GetCurve(nParticleEmitter::CurveType curveType) const
+{
+    n_assert(curveType < nParticleEmitter::CurveTypeCount);
+    n_assert(curveType >= 0);
+    return this->curves[curveType];
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+void
+nParticleShapeNode::SetRGBCurve(const nVector3EnvelopeCurve& curve)
+{
+    this->rgbCurve.SetParameters(curve);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const nVector3EnvelopeCurve&
+nParticleShapeNode::GetRGBCurve() const
+{
+    return this->rgbCurve;
+}
+
+//------------------------------------------------------------------------------
+#endif
---
+++ trunk/nebula2/code/nebula2/src/scene/nparticleshapenode_main.cc
@@ -0,0 +1,172 @@
+//------------------------------------------------------------------------------
+//  nparticleshapenode_main.cc
+//  (C) 2003 RadonLabs GmbH
+//------------------------------------------------------------------------------
+#include "scene/nparticleshapenode.h"
+#include "variable/nvariableserver.h"
+#include "scene/nrendercontext.h"
+#include "scene/nsceneserver.h"
+#include "kernel/ntimeserver.h"
+
+nNebulaScriptClass(nParticleShapeNode, "nshapenode");
+
+//------------------------------------------------------------------------------
+/**
+*/
+nParticleShapeNode::nParticleShapeNode() :
+    refParticleServer("/sys/servers/particle"),
+    refVariableServer("/sys/servers/variable"),
+    emissionDuration(10.0),
+    loop(true),
+    activityDistance(10.0f),
+    spreadAngle(0.0f),
+    birthDelay(0.0f),
+    emitterVarIndex(-1)
+{
+    int i;
+    for (i=0; i<4; i++)
+        this->curves[nParticleEmitter::ParticleVelocityFactor].keyFrameValues[i] = 1.0;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+nParticleShapeNode::~nParticleShapeNode()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+    Compute the resulting modelview matrix and set it in the scene
+    server as current modelview matrix.
+*/
+bool
+nParticleShapeNode::RenderTransform(nSceneServer* sceneServer, 
+                                nRenderContext* renderContext, 
+                                const matrix44& parentMatrix)
+{
+    n_assert(sceneServer);
+    n_assert(renderContext);
+    this->InvokeTransformAnimators(renderContext);
+    this->UpdateMatrix();
+    sceneServer->SetModelTransform(matrix44());
+    return true;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nParticleShapeNode::RenderContextCreated(nRenderContext* renderContext)
+{
+    nShapeNode::RenderContextCreated(renderContext);
+
+    // see if resources need to be reloaded
+    if (!this->AreResourcesValid())
+    {
+        this->LoadResources();
+    }
+
+    nParticleEmitter* emitter = this->refParticleServer.get()->NewParticleEmitter();
+    n_assert(0 != emitter);
+
+    // put emitter key in render context
+    this->emitterVarIndex = renderContext->AddLocalVar(nVariable(0, emitter->GetKey()));
+}
+
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nParticleShapeNode::Attach(nSceneServer* sceneServer, nRenderContext* renderContext)
+{
+    nShapeNode::Attach(sceneServer, renderContext);
+
+    // get emitter from render context
+    nVariableServer* varServer = this->refVariableServer.get();
+    nVariable& varEmitter = renderContext->GetLocalVar(this->emitterVarIndex);
+    int emitterKey = varEmitter.GetInt();
+    nParticleEmitter* emitter = this->refParticleServer->GetParticleEmitter(emitterKey);
+ 
+    // keep emitter alive
+    if (0 != emitter)
+    {
+        emitter->KeepAlive();
+    }
+    else
+    {
+        // need new emitter
+        emitter = this->refParticleServer.get()->NewParticleEmitter();
+        n_assert(0 != emitter);
+
+        varEmitter.SetInt(emitter->GetKey());
+    }
+
+    // setup emitter
+    emitter->SetMeshGroupIndex(this->groupIndex);
+    emitter->SetEmitterMesh(this->refMesh.get());
+    emitter->SetTransform(renderContext->GetTransform());
+    int windVarHandle = this->refVariableServer->GetVariableHandleByName("wind");
+    nVariable* windVar = renderContext->GetVariable(windVarHandle);
+    emitter->SetWind(windVar->GetFloat4());
+
+    // set emitter settings
+    emitter->SetEmissionDuration(this->emissionDuration);
+    emitter->SetLoop(this->loop);
+    emitter->SetActivityDistance(this->activityDistance);
+    emitter->SetSpreadAngle(this->spreadAngle);
+    emitter->SetBirthDelay(this->birthDelay);
+    emitter->SetStartRotation(this->startRotation);
+    int curveType;
+    for (curveType = 0; curveType < nParticleEmitter::CurveTypeCount; curveType++)
+    {
+        emitter->SetCurve((nParticleEmitter::CurveType) curveType, this->curves[curveType]);
+    }
+    emitter->SetRGBCurve(this->rgbCurve);
+}
+
+
+//------------------------------------------------------------------------------
+/**
+    This method must return the mesh usage flag combination required by
+    this shape node class. Subclasses should override this method
+    based on their requirements.
+
+    @return     a combination on nMesh2::Usage flags
+*/
+int
+nParticleShapeNode::GetMeshUsage() const
+{
+    return nMesh2::ReadOnly | nMesh2::PointSprite | nMesh2::NeedsVertexShader;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+nParticleShapeNode::RenderGeometry(nSceneServer* sceneServer, nRenderContext* renderContext)
+{
+    n_assert(sceneServer);
+    n_assert(renderContext);
+    nGfxServer2* gfx = this->refGfxServer.get();
+
+    nTime curTime = kernelServer->GetTimeServer()->GetTime();
+
+    // see if resources need to be reloaded
+    if (!this->AreResourcesValid())
+    {
+        this->LoadResources();
+    }
+
+    nVariableServer* varServer = this->refVariableServer.get();
+    const nVariable& varEmitter = renderContext->GetLocalVar(this->emitterVarIndex);
+    int emitterKey = varEmitter.GetInt();
+
+    nParticleEmitter* emitter = this->refParticleServer->GetParticleEmitter(emitterKey);
+    n_assert(0 != emitter);
+    emitter->Render();
+
+    return true;
+}
