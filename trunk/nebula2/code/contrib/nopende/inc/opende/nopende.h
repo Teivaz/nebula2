b09e2ab9e9697abaa50904c8670c93e7d3fac688
enlight <enlight@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-01-17 23:05:59 +0000

    Added some includes I forgot to put in originally.

--- trunk/nebula2/code/contrib/nopende/inc/opende/nopende.h
+++ trunk/nebula2/code/contrib/nopende/inc/opende/nopende.h
@@ -16,5 +16,29 @@
 #include "opende/nopendemarshal.h"
 #include "opende/nopendelayer.h"
 
+// include all the core classes for convenience
+
+#include "opende/nopendeamotorjoint.h"
+#include "opende/nopendeballjoint.h"
+#include "opende/nopendefixedjoint.h"
+#include "opende/nopendehinge2joint.h"
+#include "opende/nopendehingejoint.h"
+#include "opende/nopendesliderjoint.h"
+#include "opende/nopendeuniversaljoint.h"
+
+#include "opende/nopendebody.h"
+
+#include "opende/nopendesimplespace.h"
+#include "opende/nopendehashspace.h"
+#include "opende/nopendequadtreespace.h"
+
+#include "opende/nopendeboxgeom.h"
+#include "opende/nopendecapsulegeom.h"
+#include "opende/nopendeplanegeom.h"
+#include "opende/nopenderaygeom.h"
+#include "opende/nopendespheregeom.h"
+#include "opende/nopendetrimeshgeom.h"
+#include "opende/nopendetransformgeom.h"
+
 //----------------------------------------------------------------------------
 #endif // N_OPENDE_H
