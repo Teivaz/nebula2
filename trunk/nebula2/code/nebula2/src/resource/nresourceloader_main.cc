9a1813855cffa8634d321d3ed71006826dc73ae1
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-04-11 20:42:57 +0000

    Documentation fix.

--- trunk/nebula2/code/nebula2/src/resource/nresourceloader_main.cc
+++ trunk/nebula2/code/nebula2/src/resource/nresourceloader_main.cc
@@ -24,12 +24,12 @@ nResourceLoader::~nResourceLoader()
 /**
     Base Load() function for the nResourceLoader.  This is to be redefined by child classes
 
-    @param path             the full path to the to-be-loaded file
+    @param filename         the full path to the to-be-loaded file
     @param callingResource  ptr to the nResource calling nResourceLoader::Load()
 
     @return                 success/failure
 */
-bool nResourceLoader::Load(const char *sFilename, nResource *callingResource)
+bool nResourceLoader::Load(const char *filename, nResource *callingResource)
 {
     return false;
 }
