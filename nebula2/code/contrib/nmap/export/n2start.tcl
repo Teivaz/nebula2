d60fca2f10daf2bf43cf632dcae069fbe12038df
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-04-16 08:48:54 +0000

    Updated demo files for nMap

--- nebula2/code/contrib/nmap/export/n2start.tcl
+++ nebula2/code/contrib/nmap/export/n2start.tcl
@@ -1,3 +1,38 @@
+# copied from $home/bin/startup.tcl
+# because I don't know how to include
+# files in tcl
+
+set oldCwd [psel]
+
+#-------------------------------------------------------------------------------
+#   set up resource assigns
+#-------------------------------------------------------------------------------
+sel /sys/servers/file2
+    set proj [.manglepath "proj:"]
+    set home [.manglepath "home:"]
+    
+    .setassign "meshes"   "$proj/export/meshes/"
+    .setassign "textures" "$proj/export/textures/"
+    if {[exists /sys/servers/gfx]} {
+        if {[/sys/servers/gfx.getfeatureset] == "dx9"} {   
+            .setassign "shaders" "$home/export/shaders/2.0/"
+            puts "Shader directory: $home/export/shaders/2.0"
+        } else {
+            .setassign "shaders" "$home/export/shaders/fixed/"
+            puts "Shader directory: $home/export/shaders/fixed"
+        }
+    } else {
+        .setassign "shaders" "$home/export/shaders/2.0/"
+        puts "Shader directory: $home/export/shaders/2.0"
+    }
+    .setassign "anims"    "$proj/export/anims/"
+    .setassign "gfxlib"   "$proj/export/gfxlib/"
+
+#-------------------------------------------------------------------------------
+#   restore original directory
+#-------------------------------------------------------------------------------
+sel $oldCwd
+
 sel /usr/scene
 .setactive true
 
@@ -15,10 +50,9 @@ sel /usr/scene
 new ntransformnode light1
     sel light1
     .seteuler 0 -45 45
-    new nspotlightnode l
+    .setposition -50 5 50
+    new nlightnode l
         sel l
-        .setposition -50 5 50
-        .setscale 1000 1000 1000
         .setvector LightDiffuse 0.5 0.5 0.5 0.5
         .setvector LightSpecular 0.5 0.5 0.5 0.5
         .setvector LightAmbient 1 1 1 0.5
