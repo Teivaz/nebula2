c917bd0a3d5000e1668a83a857fb704a16486493
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-04-19 16:12:22 +0000

    new merge from RadonLabs

---
+++ nebula2/code/nebula2/inc/gfx2/nd3d9windowhandler.h
@@ -0,0 +1,37 @@
+#ifndef N_D3D9WINDOWHANDLER_H
+#define N_D3D9WINDOWHANDLER_H
+//------------------------------------------------------------------------------
+/**
+    A window handler for the d3d9 graphics server class.
+    This just implements some Direct3D specific callbacks which are called
+    from the nWin32WindowHandler's WinProc.
+    
+    (C) 2004 RadonLabs GmbH
+*/
+#include "gfx2/nwin32windowhandler.h"
+
+class nD3D9Server;
+
+//------------------------------------------------------------------------------
+class nD3D9WindowHandler : public nWin32WindowHandler
+{
+public:
+    /// constructor
+    nD3D9WindowHandler(nD3D9Server* ptr);
+    /// destructor
+    ~nD3D9WindowHandler();
+    /// called by WinProc on WM_SETCURSOR
+    virtual bool OnSetCursor();
+    /// called by WinProc on WM_PAINT
+    virtual void OnPaint();
+    /// called when fullscreen/windowed should be toggled
+    virtual void OnToggleFullscreenWindowed();
+    /// called when WM_CLOSE is received, return true if app should quit
+    virtual bool OnClose();
+
+private:
+    nD3D9Server* d3d9Server;
+};
+
+//------------------------------------------------------------------------------
+#endif    
