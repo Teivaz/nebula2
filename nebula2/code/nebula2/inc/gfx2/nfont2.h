0b6dbc29289ed93baa475afab29cce8a0ef0929e
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-04-19 15:28:17 +0000

    new merge from RadonLabs

--- nebula2/code/nebula2/inc/gfx2/nfont2.h
+++ nebula2/code/nebula2/inc/gfx2/nfont2.h
@@ -15,6 +15,19 @@
 class nFont2 : public nResource
 {
 public:
+    /// rendering flags (apply to nGfxServer2::DrawText())
+    enum RenderFlags
+    {
+        Bottom     = (1<<0),    // bottom-align text
+        Top        = (1<<1),    // top-align text
+        Center     = (1<<1),    // centers text horizontally
+        Left       = (1<<2),    // align text to the left
+        Right      = (1<<3),    // align text to the right
+        VCenter    = (1<<4),    // center text vertically
+        NoClip     = (1<<5),    // don't clip text (faster)
+        ExpandTabs = (1<<6),    // expand tabs, doh
+    };
+
     /// constructor
     nFont2();
     /// destructor
