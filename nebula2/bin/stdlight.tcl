baf1afaa7c361f0a5ed961527fa81e15cf02845b
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-04-23 13:30:26 +0000

    Merge a change from Radon Labs:  * This is important to have alpha be 1 because it is now used in    default.fx's psLighting() and can result in oddly transparent    models.

--- nebula2/bin/stdlight.tcl
+++ nebula2/bin/stdlight.tcl
@@ -14,7 +14,7 @@ new ntransformnode stdlight
         .setorthogonal 1000 1000 0.25 175
         .setvector LightDiffuse 1 1 1 1
         .setvector LightSpecular 1 1 1 1
-        .setvector LightAmbient 0 0 0 0
+        .setvector LightAmbient 0.25 0.25 0.25 1
         .settexture LightModMap "textures:system/white.dds"
     sel ..    
 sel ..    
