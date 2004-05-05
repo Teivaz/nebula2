927064cbffa8dc5a239ab06feef9a905a952a377
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-05-05 12:53:25 +0000

    Removed commented out code

--- nebula2/code/contrib/physdemo/src/physdemo/simpleobject_main.cc
+++ nebula2/code/contrib/physdemo/src/physdemo/simpleobject_main.cc
@@ -17,12 +17,6 @@ SimpleObject::SimpleObject()
 */
 SimpleObject::~SimpleObject()
 {
-    // not needed as objects are children and will be released by nRoot anyway.
-//    if (this->refPhysGeom.isvalid())
-//        this->refPhysGeom->Release();
-//    if (this->refPhysBody.isvalid())
-//        this->refPhysBody->Release();
-
     if (this->refFloatyText.isvalid())
         this->refFloatyText->Release();
 }
