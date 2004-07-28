69bee9e712e624207160c89ab49982b4f2a7d8f4
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-07-28 08:13:35 +0000

    GetKeyAt() returns an int, not a long int.

--- trunk/nebula2/code/nebula2/src/kernel/nclass.cc
+++ trunk/nebula2/code/nebula2/src/kernel/nclass.cc
@@ -194,7 +194,7 @@ nClass::EndCmds(void)
             nCmdProto *cp0 = (nCmdProto *) this->cmdTable->GetElementAt(i);
             nCmdProto *cp1 = (nCmdProto *) this->cmdTable->GetElementAt(i+1);
             n_error("Command id collision in class '%s'\n"
-                     "cmd '%s' and cmd '%s' both have id '0x%lx'\n",
+                     "cmd '%s' and cmd '%s' both have id '0x%x'\n",
                      this->GetName(),
                      cp0->GetName(),
                      cp1->GetName(),
