fc2c7ef5c4e290e11a14ed1aa3599041ce921997
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-06 13:25:25 +0000

    A simple example of nnetwork usage, from Luis Cabellos (Tragnarion Studios).
    Note: I changed this slightly to be part of the nnetwork contrib instead of a seperate contribution. It has some trouble with the project files, and crashes at startup. Those problems may well be something to do with the changes I made but I wanted to get it in so that others could sort it out if it takes me too long.

---
+++ trunk/nebula2/code/contrib/nnetwork/src/networkdemo/netserverdemo.cc
@@ -0,0 +1,39 @@
+/**
+   @file netserverdemo.cc
+   @author Luis Jose Cabellos Gomez <luis.cabellos@tragnarion.com>
+   @brief Definition of NetServerDemo class.
+   @brief $Id$ 
+
+   This file is licensed under the terms of the Nebula License.
+*/
+//------------------------------------------------------------------------------
+#include "networkdemo/netserverdemo.h"
+#include "kernel/nkernelserver.h"
+
+nNebulaClass( NetServerDemo, "nnetserver" );
+
+//------------------------------------------------------------------------------
+/**
+    Handle a custom message. Print the message and resend to the others clients
+*/
+bool
+NetServerDemo::HandleMessage(int fromClientId, const char* msg)
+{
+    n_assert(msg);
+    n_printf("NetServerDemo:message(%s)from(%d)\n", msg, fromClientId);
+    int i;
+    char msgString[1024];
+    sprintf(msgString, "~msgfrom %d %s", fromClientId, msg);
+    for (i = 0; i < this->GetNumClients(); i++)
+    {
+        if ( i!=fromClientId && (Connected == this->clientArray[i].GetClientStatus()))
+        {
+            this->ipcServer->Send(i, nIpcBuffer(msgString));
+        }
+    }
+    this->ipcServer->Send(fromClientId, nIpcBuffer("~msg received"));
+    return true;
+}
+//------------------------------------------------------------------------------
+//   EOF 
+//------------------------------------------------------------------------------
