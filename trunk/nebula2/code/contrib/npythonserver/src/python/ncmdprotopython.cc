df516773ffb268384f694cf95105d82d157bda62
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-07 05:22:25 +0000

    Added to support cmdproto of npython server

---
+++ trunk/nebula2/code/contrib/npythonserver/src/python/ncmdprotopython.cc
@@ -0,0 +1,41 @@
+//------------------------------------------------------------------------------
+//   ncmdprotopython.cc
+//
+//   (C)2004 Kim, Hyoun Woo.
+//------------------------------------------------------------------------------
+#include "python/ncmdprotopython.h"
+#include "python/npythonserver.h"
+
+//------------------------------------------------------------------------------
+/**
+    Constructor.
+*/
+nCmdProtoPython::nCmdProtoPython(const char* protoDef)
+    : nCmdProto(protoDef, 0)
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+    Copy-constructor.
+*/
+nCmdProtoPython::nCmdProtoPython(const nCmdProtoPython& rhs)
+    : nCmdProto(rhs)
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+    Dispatch specified command.
+
+    @param obj pointer to nebula object(derives nRoot)
+    @cmd pointer to nCmd which contains in-args and out-args.
+*/
+bool nCmdProtoPython::Dispatch(void* obj, nCmd* cmd)
+{
+    const char* result;
+    const char* funcName = cmd->GetProto()->GetName();
+
+    return nPythonServer::Instance->RunFunction(funcName, result);
+}
+
