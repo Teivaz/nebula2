34db9ed9284653b961c3014faa15a5538702c15e
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-09 16:10:42 +0000

    Added many useful script commands.

--- nebula2/code/nebula2/src/gui/nguihorislidergroup_cmds.cc
+++ nebula2/code/nebula2/src/gui/nguihorislidergroup_cmds.cc
@@ -5,6 +5,14 @@
 #include "gui/nguihorislidergroup.h"
 
 static void n_setlefttext(void* slf, nCmd* cmd);
+static void n_setrighttext(void* slf, nCmd* cmd);
+static void n_setminvalue(void* slf, nCmd* cmd);
+static void n_setmaxvalue(void* slf, nCmd* cmd);
+static void n_setvalue(void* slf, nCmd* cmd);
+static void n_getvalue(void* slf, nCmd* cmd);
+static void n_setleftwidth(void* slf, nCmd* cmd);
+static void n_setrightwidth(void* slf, nCmd* cmd);
+static void n_setlabelfont(void* slf, nCmd* cmd);
 //-----------------------------------------------------------------------------
 /**
     @scriptclass
@@ -26,6 +34,14 @@ n_initcmds(nClass* cl)
 {
     cl->BeginCmds();
     cl->AddCmd("v_setlefttext_s",    'SLFT', n_setlefttext);
+    cl->AddCmd("v_setrighttext_s",   'SRGT', n_setrighttext);
+    cl->AddCmd("v_setminvalue_i",    'SMIN', n_setminvalue);
+    cl->AddCmd("v_setmaxvalue_i",    'SMAX', n_setmaxvalue);
+    cl->AddCmd("v_setvalue_i",       'SVAL', n_setvalue);
+    cl->AddCmd("i_getvalue_v",       'GVAL', n_getvalue);
+    cl->AddCmd("v_setleftwidth_f",   'SLWD', n_setleftwidth);
+    cl->AddCmd("v_setrightwidth_f",  'SFWD', n_setrightwidth);
+    cl->AddCmd("v_setlabelfont_s",   'SFON', n_setlabelfont);
     cl->EndCmds();
 }
 
@@ -34,15 +50,151 @@ n_initcmds(nClass* cl)
     @cmd
     setlefttext
     @input
-    s(text)
+    s(a printf formatted string)
     @output
     v
     @info
-    Set the name of the cursor brush.
+    Set text to left of slider.
 */
 static void
 n_setlefttext(void* slf, nCmd* cmd)
 {
     nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
     self->SetLeftText(cmd->In()->GetS());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setrighttext
+    @input
+    s(a printf formatted string)
+    @output
+    v
+    @info
+    Set text to right of slider.
+*/
+static void
+n_setrighttext(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetRightText(cmd->In()->GetS());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setminvalue
+    @input
+    i
+    @output
+    v
+    @info
+    Sets the slider's minimum value.
+*/
+static void
+n_setminvalue(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetMinValue(cmd->In()->GetI());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setmaxvalue
+    @input
+    i
+    @output
+    v
+    @info
+    Sets the slider's maximum value.
+*/
+static void
+n_setmaxvalue(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetMaxValue(cmd->In()->GetI());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setvalue
+    @input
+    i
+    @output
+    v
+    @info
+    Sets the slider's current value.
+*/
+static void
+n_setvalue(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetValue(cmd->In()->GetI());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    getvalue
+    @input
+    v
+    @output
+    i
+    @info
+    Gets the slider's current value.
+*/
+static void
+n_getvalue(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    cmd->Out()->SetI(self->GetValue());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setleftwidth
+    @input
+    f
+    @output
+    v
+    @info
+    Sets relative width of left text label.
+*/
+static void
+n_setleftwidth(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetLeftWidth(cmd->In()->GetF());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setrightwidth
+    @input
+    f
+    @output
+    v
+    @info
+    Sets relative width of right text label.
+*/
+static void
+n_setrightwidth(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetRightWidth(cmd->In()->GetF());
+}
+//-----------------------------------------------------------------------------
+/**
+    @cmd
+    setlabelfont
+    @input
+    f
+    @output
+    v
+    @info
+    Sets the font for the left & right labels.
+*/
+static void
+n_setlabelfont(void* slf, nCmd* cmd)
+{
+    nGuiHoriSliderGroup* self = (nGuiHoriSliderGroup*) slf;
+    self->SetLabelFont(cmd->In()->GetS());
 }
