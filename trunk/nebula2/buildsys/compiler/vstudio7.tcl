db42687ed4d4505008acf07a971c4075d58e5dc1
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-10 11:56:53 +0000

    Custom resource files are now added to the Resource Files folder, along with the default exe resource file. works with vc7, vc71 and vc8.

--- trunk/nebula2/buildsys/compiler/vstudio7.tcl
+++ trunk/nebula2/buildsys/compiler/vstudio7.tcl
@@ -216,23 +216,29 @@ proc emit_vcproj_files {name cid} {
     }
     
     # Resource files
-    if {[get_tartype $name] == "exe"} {
+    set tartype [get_tartype $name]
+    if {$tartype == "exe" || $tartype == "dll"} {        
         puts $cid "\t\t<Filter Name=\"Resource Files\" Filter=\"rc\" >"
-        puts $cid "\t\t\t<File RelativePath=\"pkg\\res_$name.rc\" />"
+
+        # add standard nebula rsrc to exe
+        if {$tartype == "exe"} {
+            puts $cid "\t\t\t<File RelativePath=\"pkg\\res_$name.rc\" />"
+        }
+        
+        # add any custom rsrc files
+        set resfile [get_win32resource $name]
+        if { $resfile != "" } {
+            global cur_workspacepath
+            global home
+            global mod
+    
+            set startpath [string trim $home '/']
+            set i [findmodbyname $name]
+            set resfile [findrelpath $cur_workspacepath $startpath/code/$mod($i,trunkdir)/res/$resfile.rc]
+            regsub -all "/" $resfile "\\" filename
+            puts $cid "\t\t\t<File RelativePath=\"$filename\" />"
+        }
         puts $cid "\t\t</Filter>"
-    }
-
-    set resfile [get_win32resource $name]
-    if { $resfile != "" } {
-        global cur_workspacepath
-        global home
-        global mod
-
-        set startpath [string trim $home '/']
-        set i [findmodbyname $name]
-        set resfile [findrelpath $cur_workspacepath $startpath/code/$mod($i,trunkdir)/res/$resfile.rc]
-        regsub -all "/" $resfile "\\" filename
-        puts $cid "\t\t<File RelativePath=\"$filename\" />"
     }
 
     puts $cid "\t</Files>"
