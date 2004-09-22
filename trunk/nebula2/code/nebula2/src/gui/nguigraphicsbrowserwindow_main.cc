e046401ebd63bcd908fdb1713b0b3e09432678b2
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-22 15:18:34 +0000

    Adds/Deletes for Radon merge.

---
+++ trunk/nebula2/code/nebula2/src/gui/nguigraphicsbrowserwindow_main.cc
@@ -0,0 +1,159 @@
+//------------------------------------------------------------------------------
+//  nguigraphicsbrowserwindow_main.cc
+//  (C) 2004 RadonLabs GmbH
+//------------------------------------------------------------------------------
+#include "gui/nguigraphicsbrowserwindow.h"
+#include "kernel/ntimeserver.h"
+
+nNebulaClass(nGuiGraphicsBrowserWindow, "nguiclientwindow");
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiGraphicsBrowserWindow::nGuiGraphicsBrowserWindow() :
+    refScriptServer("/sys/servers/script"),
+    refUsrScene("/usr/scene")
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiGraphicsBrowserWindow::~nGuiGraphicsBrowserWindow()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiGraphicsBrowserWindow::OnShow()
+{
+    nGuiSkin* skin = nGuiServer::Instance()->GetSkin();
+    n_assert(skin);
+
+    // call parent class
+    nGuiClientWindow::OnShow();
+
+    // set the window title
+    this->SetTitle("Graphics Object Browser");
+
+    // get client area form layout object
+    nGuiFormLayout* layout = this->refFormLayout.get();
+    kernelServer->PushCwd(layout);
+    vector2 buttonSize = nGuiServer::Instance()->ComputeScreenSpaceBrushSize("button_n");
+
+    // create load button
+    nGuiTextButton* loadButton = (nGuiTextButton*) kernelServer->New("nguitextbutton", "LoadButton");
+    n_assert(loadButton);
+    loadButton->SetText("Load");
+    loadButton->SetFont("GuiSmall");
+    loadButton->SetAlignment(nGuiTextButton::Center);
+    loadButton->SetDefaultBrush("button_n");
+    loadButton->SetPressedBrush("button_p");
+    loadButton->SetHighlightBrush("button_h");
+    loadButton->SetMinSize(buttonSize);
+    loadButton->SetMaxSize(buttonSize);
+    loadButton->SetColor(skin->GetButtonTextColor());
+    layout->AttachForm(loadButton, nGuiFormLayout::Right, 0.005f);
+    layout->AttachForm(loadButton, nGuiFormLayout::Bottom, 0.005f);
+    this->refLoadButton = loadButton;
+
+    // create reset time button
+    nGuiTextButton* timeButton = (nGuiTextButton*) kernelServer->New("nguitextbutton", "TimeButton");
+    n_assert(timeButton);
+    timeButton->SetText("Reset Time");
+    timeButton->SetFont("GuiSmall");
+    timeButton->SetAlignment(nGuiTextButton::Center);
+    timeButton->SetDefaultBrush("button_n");
+    timeButton->SetPressedBrush("button_p");
+    timeButton->SetHighlightBrush("button_h");
+    timeButton->SetMinSize(buttonSize);
+    timeButton->SetMaxSize(buttonSize);
+    timeButton->SetColor(skin->GetButtonTextColor());
+    layout->AttachWidget(timeButton, nGuiFormLayout::Right, loadButton, 0.005f);
+    layout->AttachForm(timeButton, nGuiFormLayout::Bottom, 0.005f);
+    this->refTimeButton = timeButton;
+
+    // create category browser
+    nGuiCategoryBrowser* catBrowser = (nGuiCategoryBrowser*) kernelServer->New("nguicategorybrowser", "CatBrowser");
+    n_assert(catBrowser);
+    catBrowser->SetDirectory("proj:export/gfxlib");
+    layout->AttachForm(catBrowser, nGuiFormLayout::Top, 0.005f);
+    layout->AttachForm(catBrowser, nGuiFormLayout::Left, 0.005f);
+    layout->AttachForm(catBrowser, nGuiFormLayout::Right, 0.005f);
+    layout->AttachWidget(catBrowser, nGuiFormLayout::Bottom, loadButton, 0.005f);
+    this->refCatBrowser = catBrowser;
+
+    // need to invoke on show manually on layout
+    layout->OnShow();
+
+    kernelServer->PopCwd();
+
+    // update all layouts
+    this->UpdateLayout(this->rect);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiGraphicsBrowserWindow::OnHide()
+{
+    this->refLoadButton->Release();
+    this->refTimeButton->Release();
+    this->refCatBrowser->Release();
+    nGuiClientWindow::OnHide();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiGraphicsBrowserWindow::OnEvent(const nGuiEvent& event)
+{
+    if ((event.GetType() == nGuiEvent::ButtonUp) && (event.GetWidget() == this->refLoadButton))
+    {
+        // load button pressed?
+        nString objPath = this->refCatBrowser->GetSelectedPath();
+        if (!objPath.IsEmpty())
+        {
+            this->LoadObject(objPath);
+        }
+    }
+    else if ((event.GetType() == nGuiEvent::ButtonUp) && (event.GetWidget() == this->refTimeButton))
+    {
+        // reset time
+        nTimeServer::Instance()->ResetTime();
+    }
+
+    // hand to parent class
+    nGuiClientWindow::OnEvent(event);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiGraphicsBrowserWindow::LoadObject(const nString& objPath)
+{
+    n_assert(!objPath.IsEmpty());
+
+    // clear usr/scene
+    this->refUsrScene->Release();
+    kernelServer->New("ntransformnode", "/usr/scene");
+
+    // source the light stage...
+    const char* result;
+    this->refScriptServer->RunScript("home:bin/stdlight.tcl", result);
+
+    // load new object
+    kernelServer->PushCwd(this->refUsrScene);
+    kernelServer->Load(objPath.Get());
+    kernelServer->PopCwd();
+
+    // reset time
+    nTimeServer::Instance()->ResetTime();
+}
---
+++ trunk/nebula2/code/nebula2/src/gui/nguitextwindow_main.cc
@@ -0,0 +1,150 @@
+//------------------------------------------------------------------------------
+//  nguitextwindow_main.cc
+//  (C) 2004 RadonLabs GmbH
+//------------------------------------------------------------------------------
+#include "gui/nguitextwindow.h"
+#include "gui/nguitextview.h"
+#include "gui/nguitextbutton.h"
+#include "kernel/nfileserver2.h"
+
+nNebulaClass(nGuiTextWindow, "nguiclientwindow");
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiTextWindow::nGuiTextWindow()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiTextWindow::~nGuiTextWindow()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiTextWindow::OnShow()
+{
+    nGuiSkin* skin = nGuiServer::Instance()->GetSkin();
+    n_assert(skin);
+
+    // call parent class
+    nGuiClientWindow::OnShow();
+
+    rectangle windowRect(vector2(0.15f, 0.15f), vector2(0.85f, 0.85f));
+    this->SetRect(windowRect);
+
+    // get client area form layout object
+    nGuiFormLayout* layout = this->refFormLayout.get();
+    kernelServer->PushCwd(layout);
+
+    vector2 buttonSize = nGuiServer::Instance()->ComputeScreenSpaceBrushSize("button_n");
+
+    // close button
+    nGuiTextButton* closeButton = (nGuiTextButton*) kernelServer->New("nguitextbutton", "CloseButton");
+    n_assert(closeButton);
+    closeButton->SetText("Close");
+    closeButton->SetFont("GuiSmall");
+    closeButton->SetAlignment(nGuiTextButton::Center);
+    closeButton->SetDefaultBrush("button_n");
+    closeButton->SetPressedBrush("button_p");
+    closeButton->SetHighlightBrush("button_h");
+    closeButton->SetMinSize(buttonSize);
+    closeButton->SetMaxSize(buttonSize);
+    closeButton->SetColor(skin->GetButtonTextColor());
+    layout->AttachForm(closeButton, nGuiFormLayout::Right, 0.005f);
+    layout->AttachForm(closeButton, nGuiFormLayout::Bottom, 0.005f);
+    closeButton->OnShow();
+    this->refCloseButton = closeButton;
+
+    // text view
+    nGuiTextView* textView = (nGuiTextView*) kernelServer->New("nguitextview", "TextView");
+    n_assert(textView);
+    textView->SetDefaultBrush("list_background");
+    textView->SetFont("GuiDefault");
+    layout->AttachForm(textView, nGuiFormLayout::Top, 0.005f);
+    layout->AttachForm(textView, nGuiFormLayout::Left, 0.005f);
+    layout->AttachForm(textView, nGuiFormLayout::Right, 0.005f);
+    layout->AttachWidget(textView, nGuiFormLayout::Bottom, closeButton, 0.005f);
+    textView->OnShow();
+    this->refTextView = textView;
+
+    kernelServer->PopCwd();
+
+    // read file into text view
+    this->UpdateTextView();
+
+    // update all layouts
+    this->UpdateLayout(this->rect);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiTextWindow::OnHide()
+{
+    if (this->refTextView.isvalid())
+    {
+        this->refTextView->Release();
+    }
+    if (this->refCloseButton.isvalid())
+    {
+        this->refCloseButton->Release();
+    }
+    nGuiClientWindow::OnHide();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiTextWindow::OnEvent(const nGuiEvent& event)
+{
+    if ((event.GetType() == nGuiEvent::ButtonUp) && (event.GetWidget() == this->refCloseButton))
+    {
+        this->SetCloseRequested(true);
+    }
+    nGuiClientWindow::OnEvent(event);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiTextWindow::UpdateTextView()
+{
+    char buf[1024];
+    nGuiTextView* textView = this->refTextView;
+
+    this->refTextView->BeginAppend();
+    if (this->filename.IsEmpty())
+    {
+        textView->AppendLine("No filename!");
+    }
+    else
+    {
+        nFile* file = nFileServer2::Instance()->NewFileObject();
+        if (file->Open(this->filename.Get(), "r"))
+        {
+            while (file->GetS(buf, sizeof(buf)))
+            {
+                textView->AppendLine(buf);
+            }
+            file->Close();
+        }
+        else
+        {
+            snprintf(buf, sizeof(buf), "Could not open text file '%s'!", this->filename.Get());
+            textView->AppendLine(buf);
+        }
+        file->Release();
+    }
+    this->refTextView->EndAppend();
+}
