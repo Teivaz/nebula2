d2aaf7899fc238f6f15af90cb4dbd4a75b8da5a5
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-22 14:12:17 +0000

    Merge Radon Labs changes.

--- trunk/nebula2/code/nebula2/src/variable/nvariableserver_main.cc
+++ trunk/nebula2/code/nebula2/src/variable/nvariableserver_main.cc
@@ -4,7 +4,6 @@
 //------------------------------------------------------------------------------
 #include "variable/nvariableserver.h"
 #include "variable/nvariablecontext.h"
-#include "kernel/nkernelserver.h"
 
 nNebulaScriptClass(nVariableServer, "nroot");
 nVariableServer* nVariableServer::Singleton = 0;
