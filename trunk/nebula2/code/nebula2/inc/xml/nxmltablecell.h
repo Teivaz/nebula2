c0c6c16bfedc216ddc424e371bc07c9e3ba29343
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-23 15:27:26 +0000

    Radon merge.

---
+++ trunk/nebula2/code/nebula2/inc/xml/nxmltablecell.h
@@ -0,0 +1,194 @@
+#ifndef N_XMLTABLECELL_H
+#define N_XMLTABLECELL_H
+//------------------------------------------------------------------------------
+/**
+    @class nXmlTableCell
+    @ingroup Xml
+
+    A single cell in an XML table. The primary representation is as string,
+    but bool, int and float values can be returned (converted values are
+    cached for efficiency). Conversion happens during Set().
+    
+    (C) 2004 RadonLabs GmbH
+*/
+#include "kernel/ntypes.h"
+#include "mathlib/vector.h"
+
+//------------------------------------------------------------------------------
+class nXmlTableCell
+{
+public:
+    /// constructor
+    nXmlTableCell();
+    /// destructor
+    ~nXmlTableCell();
+    /// set value
+    void Set(const char* s);
+    /// get value
+    const char* Get() const;
+    /// get value as nString
+    const nString& AsString() const;
+    /// get value as int
+    int AsInt() const;
+    /// get value as float
+    float AsFloat() const;
+    /// get value as bool
+    bool AsBool() const;
+    /// get value as vector3
+    const vector3& AsVector3() const;
+    /// get value as vector4
+    const vector4& AsVector4() const;
+
+private:
+    nString valueAsString;
+    int valueAsInt;
+    float valueAsFloat;
+    bool valueAsBool;
+    float valueAsVector[4];
+};
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+nXmlTableCell::nXmlTableCell() :
+    valueAsString("<invalid>"),
+    valueAsInt(0),
+    valueAsFloat(0),
+    valueAsBool(false)
+{
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        this->valueAsVector[i] = 0.0f;
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+nXmlTableCell::~nXmlTableCell()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+    01-Jun-04   added UTF-8 to ANSI-conversion
+*/
+inline
+void
+nXmlTableCell::Set(const char* str)
+{
+    n_assert(str);
+
+    this->valueAsString = str;
+    this->valueAsString.UTF8toANSI();
+
+    this->valueAsFloat  = float(atof(str));
+    this->valueAsInt    = atoi(str);
+    this->valueAsBool   = false;
+
+    nArray<nString> tokens;
+    this->valueAsString.Tokenize(" ,", tokens);
+    int i;
+    for (i = 0; (i < 4) && (i < tokens.Size()); i++)
+    {
+        this->valueAsVector[i] = float(atof(tokens[i].Get()));
+    }
+
+    // NOTE: odd indices mean "false"
+    // FIXME: this could be a real performance killer
+    static const char* boolOptions[] = {
+        "ja", "nein", "yes", "no", "ein", "aus", "on", "off", "wahr", "falsch", "true", "false", 0
+    };
+    const char* cur;
+    int index = -1;
+    while ((cur = boolOptions[i]) != 0)
+    {
+        if (0 == n_stricmp(cur, str))
+        {
+            index = i;
+            break;
+        }
+        i++;
+    }
+    if (index != -1)
+    {
+        this->valueAsBool = ((index & 1) == 0);
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const char*
+nXmlTableCell::Get() const
+{
+    return this->valueAsString.Get();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const nString&
+nXmlTableCell::AsString() const
+{
+    return this->valueAsString;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+int
+nXmlTableCell::AsInt() const
+{
+    return this->valueAsInt;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+float
+nXmlTableCell::AsFloat() const
+{
+    return this->valueAsFloat;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+bool
+nXmlTableCell::AsBool() const
+{
+    return this->valueAsBool;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const vector3&
+nXmlTableCell::AsVector3() const
+{
+    return *(const vector3*)this->valueAsVector;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline
+const vector4&
+nXmlTableCell::AsVector4() const
+{
+    return *(const vector4*)this->valueAsVector;
+}
+
+//------------------------------------------------------------------------------
+#endif
---
+++ trunk/nebula2/code/nebula2/inc/xml/xmltable.h
@@ -0,0 +1,77 @@
+#ifndef XML_XMLTABLE_H
+#define XML_XMLTABLE_H
+//------------------------------------------------------------------------------
+/**
+    @class XmlTable
+    @ingroup Obsolete
+
+    Multi key table based on an (Excel exported) xml file.
+    
+    (C) 2003-2004 RadonLabs GmbH
+*/
+
+#include "util/nstring.h"
+#include "xml/xmltablecell.h"
+
+class TiXmlDocument;
+class TiXmlElement;
+
+//------------------------------------------------------------------------------
+class XmlTable
+{
+public:
+    /// Construct
+    XmlTable();
+    /// Destruct
+    ~XmlTable();
+
+    /// Load from file associated with filename `fn'.
+    void Load(const nString& fn);
+    /// Destroy table.
+    void Unload();
+
+    /// Item at index `row', `col' as boolean. Cell must be of type `bool'.
+    bool GetBool(int row, int col) const;
+    /// Item at index `row', `col' as float. Cell must be of type `float'.
+    float GetFloat(int row, int col) const;
+    /// Item at index `row', `col' as integer. Cell must be of type `int'.
+    int GetInt(int row, int col) const;
+    /// Item at index `row', `col' as string. Cell must be of type `string'.
+    const nString& GetString(int row, int col) const;
+
+    /// Name of file table is loaded from.
+    const nString& GetFileName() const;
+    /// Table height.
+    int GetHeight() const;
+    /// Table width.
+    int GetWidth() const;
+
+    /// Is table initialized?
+    bool Loaded() const;
+    /// Is index (i, j) a valid index?
+    bool ValidIndex(int row, int col) const;
+
+private:
+    /// Reset to initial values.
+    void Reset();
+    /// Make table from xml file.
+    void Setup(const nString& filename);
+    /// Update table measures from values in `document'.
+    void UpdateTableMeasures(TiXmlDocument* document);
+    /// Extract data from xml document.
+    void UpdateTableData(TiXmlDocument* document);
+
+    /// Update table cell `cell' with values from xml cell `xmlCell'.
+    void UpdateCell(XmlTableCell* cell, TiXmlElement* xmlCell);
+    /// Value `cell' is containing as string.
+    const char* GetCellValue(TiXmlElement* cell) const;
+
+    // Data
+    nString filename;
+    XmlTableCell* table;
+    int height;
+    int width;
+};
+
+//------------------------------------------------------------------------------
+#endif
