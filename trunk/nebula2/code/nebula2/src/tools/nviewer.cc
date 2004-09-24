45f302d50796059aa04ea1b7a1811aec1a7877d1
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-24 06:57:47 +0000

    Change default scene server to nmrtsceneserver for now.

--- trunk/nebula2/code/nebula2/src/tools/nviewer.cc
+++ trunk/nebula2/code/nebula2/src/tools/nviewer.cc
@@ -68,7 +68,7 @@ main(int argc, const char** argv)
 #endif
 
     const char* scriptserverArg = args.GetStringArg("-scriptserver", "ntclserver");
-    const char* sceneserverArg = args.GetStringArg("-sceneserver", "nstdsceneserver");
+    const char* sceneserverArg = args.GetStringArg("-sceneserver", "nmrtsceneserver");
     const char* startupArg = args.GetStringArg("-startup", "home:bin/startup.tcl");
     const char* viewArg   = args.GetStringArg("-view", 0);
     const char* stageArg  = args.GetStringArg("-stage", "home:bin/stdlight.tcl");
