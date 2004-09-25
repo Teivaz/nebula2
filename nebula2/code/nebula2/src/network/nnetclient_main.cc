feedfd3e30950ae250d337fa5af7d68577ff4ac0
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-25 15:33:55 +0000

    Warning fix.

--- nebula2/code/nebula2/src/network/nnetclient_main.cc
+++ nebula2/code/nebula2/src/network/nnetclient_main.cc
@@ -169,7 +169,7 @@ nNetClient::Trigger()
                     }
                 }
             }
-            while (curMsg = msgBuffer.GetNextString());
+            while ((curMsg = msgBuffer.GetNextString()));
         }
     }
 }
