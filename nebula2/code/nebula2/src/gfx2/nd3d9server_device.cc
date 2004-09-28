ab111f4b4b386f70c32ca0f6b32f6fccab0f5481
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-28 07:44:46 +0000

    Renabled loading of custom cursor texture (lost during merge).  Bombsquad Bruce should now work.

--- nebula2/code/nebula2/src/gfx2/nd3d9server_device.cc
+++ nebula2/code/nebula2/src/gfx2/nd3d9server_device.cc
@@ -626,16 +626,18 @@ nD3D9Server::UpdateCursor()
     if (this->cursorDirty)
     {
         this->cursorDirty = false;
-
-        nTexture2* tex = this->curMouseCursor.GetTexture();
-        if (tex)
+	
+        if (this->curMouseCursor.GetFilename())
         {
-            if (!tex->IsValid())
+            if (!this->curMouseCursor.IsLoaded())
             {
-                bool mouseCursorLoaded = tex->Load();
+                bool mouseCursorLoaded = this->curMouseCursor.Load();
                 n_assert(mouseCursorLoaded);
                 this->cursorDirty = true;
             }
+
+            nTexture2* tex = this->curMouseCursor.GetTexture();
+            n_assert(tex && tex->IsValid());
 
             HRESULT hr;
             IDirect3DTexture9* d3d9Tex = ((nD3D9Texture*)tex)->GetTexture2D();
