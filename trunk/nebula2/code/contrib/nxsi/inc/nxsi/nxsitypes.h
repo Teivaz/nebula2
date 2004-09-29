566087d05833572c8c3d21bb333d66702a6140a1
villeruusutie <villeruusutie@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-09-29 05:59:51 +0000

    Initial checkin.

---
+++ trunk/nebula2/code/contrib/nxsi/inc/nxsi/nxsitypes.h
@@ -0,0 +1,18 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Ville Ruusutie, 2004. All Rights Reserved.
+//-----------------------------------------------------------------------------
+// See the file "nxsi_license.txt" for information on usage and redistribution
+// of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//-----------------------------------------------------------------------------
+//#include <mathlib/vector.h>
+//-----------------------------------------------------------------------------
+
+struct nXSIWeight
+{
+    float joints[4];
+    float weights[4];
+    int count;
+};
+
+//-----------------------------------------------------------------------------
+// Eof
