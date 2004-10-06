e648db2cfdd5b690ba754d8dd26c163dd795ed9c
leaf <leaf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-10-06 17:55:08 +0000

    Start of a tutorial series, more to come. Warning basic01 is really, really basic.

---
+++ trunk/nebula2/code/contrib/tutorials/src/basic/basic01.cc
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+/**
+    @file
+    @ingroup Nebula2TutorialsBasic
+    
+    @brief Basic Example 01 - Creating the Kernel Server.
+    
+    The simplest possible Nebula2 program. Creates the nKernelServer,
+    logs a message which is sent to stdout and deletes the nKernelServer.
+*/
+
+#include "kernel/nkernelserver.h"
+
+main(int argc, const char** argv)
+{
+    nKernelServer* kernelServer = new nKernelServer();
+
+    n_printf("Goodbye Cruel World");
+
+    delete kernelServer;
+    return;
+}
