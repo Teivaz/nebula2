c0df8ff3b2eaade5059f14b0571f3e8fc2ec4bc8
ghaussmann <ghaussmann@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-10-08 22:33:36 +0000

    Spiced up occlusion test configuration some more and added some more script functions for easy building of visibility test configurations.

--- nebula2/code/contrib/nspatialdb/bin/buildfuncs.lua
+++ nebula2/code/contrib/nspatialdb/bin/buildfuncs.lua
@@ -39,29 +39,103 @@ end
 -- the blocks are always of size 3x3, except the top and bottom which are
 -- sized to cover the whole area if possible
 
-function buildfence(xcenter, ycenter, zcenter, xblocks, zblocks, hastop, hasbottom)
-    blocksize = 3.0
+function buildfence(xcenter, ycenter, zcenter, xblocks, zblocks, blocksize, hastop, hasbottom)
+    r = nebula.world.rootsector
+
+    blocksizeh = blocksize/2.0
     northz = zcenter - zblocks*0.5*blocksize
     southz = zcenter + zblocks*0.5*blocksize
     westx = xcenter - xblocks*0.5*blocksize
     eastx = xcenter + xblocks*0.5*blocksize
+    upy = ycenter - blocksizeh
+    downy = ycenter + blocksizeh
+    xgapblock = math.floor(xblocks/2)
+    zgapblock = math.floor(zblocks/2)
 
     -- build the north/south sets of blocks
-    gapblock = math.floor(xblocks/2)
-    for block=1,xblocks-1 do
-        if (block ~= gapblock) then
-            buildblock(westx + block * blocksize, ycenter, northz, blocksize)
-            buildblock(westx + block * blocksize, ycenter, southz, blocksize)
+    for block=1,xblocks-2 do
+        if (block ~= xgapblock) then
+            blockx = westx + (block+0.5) * blocksize
+            blocky = ycenter
+            r:addvisibleobject('fenceblock', '/usr/occluderobject', blockx, blocky, northz+blocksizeh, blocksizeh)
+            r:addvisibleobject('fenceblock', '/usr/occluderobject', blockx, blocky, southz-blocksizeh, blocksizeh)
         end
     end
 
     -- build the east/west sets of blocks
-    gapblock = math.floor(zblocks/2)
-    for block=0,zblocks do
-        if (block ~= gapblock) then
-            buildblock(westx, ycenter, northz + block*blocksize, blocksize)
-            buildblock(eastx, ycenter, northz + block*blocksize, blocksize)
+    for block=0,zblocks-1 do
+        if (block ~= zgapblock) then
+            blocky = ycenter
+            blockz = northz + (block+0.5) * blocksize
+            r:addvisibleobject('fenceblock', '/usr/occluderobject', westx+blocksizeh, blocky, blockz, blocksizeh)
+            r:addvisibleobject('fenceblock', '/usr/occluderobject', eastx-blocksizeh, blocky, blockz, blocksizeh)
         end
     end
 
+    -- add in the occluders, 8 of them
+    r:addoccludingobject('fenceoccblock', westx, upy, northz, westx + (xgapblock) * blocksize, downy, northz + blocksize)
+    r:addoccludingobject('fenceoccblock', westx, upy, southz - blocksize, westx + (xgapblock) * blocksize, downy, southz)
+    r:addoccludingobject('fenceoccblock', westx + (xgapblock+1) * blocksize, upy, northz, eastx, downy, northz + blocksize)
+    r:addoccludingobject('fenceoccblock', westx + (xgapblock+1) * blocksize, upy, southz - blocksize, eastx, downy, southz)
+
+    r:addoccludingobject('fenceoccblock', westx, upy, northz, westx + blocksize, downy, northz + (zgapblock) * blocksize)
+    r:addoccludingobject('fenceoccblock', westx, upy, northz + (zgapblock+1) * blocksize, westx + blocksize, downy, southz)
+    r:addoccludingobject('fenceoccblock', eastx-blocksize, upy, northz, eastx, downy, northz + (zgapblock) * blocksize)
+    r:addoccludingobject('fenceoccblock', eastx-blocksize, upy, northz + (zgapblock+1) * blocksize, eastx, downy, southz)
+
+    -- add in top/bottom if needed
+    if (xblocks > zblocks) then
+        maxdim = xblocks * blocksize
+    else
+        maxdim = zblocks * blocksize
+    end
+    maxdimh = maxdim / 2.0
+    if (hastop) then
+        topycenter = ycenter - blocksizeh - maxdimh
+        r:addvisibleobject('ceilingblock','/usr/occluderobject',xcenter,topycenter,zcenter, maxdimh)
+        r:addoccludingobject('ceilingoccblock', 
+            xcenter - maxdimh, topycenter - maxdimh, zcenter - maxdimh,
+            xcenter + maxdimh, topycenter + maxdimh, zcenter + maxdimh)
+    end
+    if (hasbottom) then
+        botycenter = ycenter + blocksizeh + maxdimh
+        r:addvisibleobject('floorblock','/usr/occluderobject',xcenter,botycenter,zcenter, maxdimh)
+        r:addoccludingobject('flooroccblock', 
+            xcenter - maxdimh, botycenter - maxdimh, zcenter - maxdimh,
+            xcenter + maxdimh, botycenter + maxdimh, zcenter + maxdimh)
+    end
+
+
 end
+
+--
+-- script to build a grid of visible objects.  strew these around for dramatic visibility demos
+--
+
+--
+-- basically you specify a bounding box that wil be filled with objects, and the spacing and size of the objects
+--
+function buildgridofstuff(xlow,ylow,zlow, xhigh,yhigh,zhigh, spacing, objectsize)
+    r = nebula.world.rootsector
+
+    xobjects = math.floor((xhigh-xlow)/spacing)
+    yobjects = math.floor((yhigh-ylow)/spacing)
+    zobjects = math.floor((zhigh-zlow)/spacing)
+
+    if (xobjects < 1) then xobjects=1 end
+    if (yobjects < 1) then yobjects=1 end
+    if (zobjects < 1) then zobjects=1 end
+
+    for xobject=1,xobjects do
+      for yobject=1,yobjects do
+        for zobject=1,zobjects do
+            xc = xlow + (xobject-1) * spacing
+            yc = ylow + (yobject-1) * spacing
+            zc = zlow + (zobject-1) * spacing
+            r:addvisibleobject('showthing', '/usr/testobject', xc,yc,zc, objectsize)
+        end
+      end
+    end 
+    
+end
+
