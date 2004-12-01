4e77b45fa7b37063579e0011540ed17e501883f2
ghaussmann <ghaussmann@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2004-12-01 02:52:52 +0000

    Updated RunScript() calls in nsdbviewerapp to work with new scriptserver.

--- nebula2/code/contrib/nspatialdb/src/sdbviewer/nsdbviewerapp.cc
+++ nebula2/code/contrib/nspatialdb/src/sdbviewer/nsdbviewerapp.cc
@@ -118,7 +118,7 @@ nSDBViewerApp::Open()
     // run startup script
     if (this->GetStartupScript())
     {
-        const char* result;
+        nString result;
         this->refScriptServer->RunScript(this->GetStartupScript(), result);
     }
 
@@ -133,7 +133,7 @@ nSDBViewerApp::Open()
     this->refInputServer    = (nInputServer*)     kernelServer->New("ndi8server", "/sys/servers/input");
     if (NULL != this->GetInputScript())
     {
-        const char* result;
+        nString result;
         this->refScriptServer->RunScript(this->GetInputScript(), result);
     }
 
@@ -155,7 +155,7 @@ nSDBViewerApp::Open()
     if (this->GetSceneFile())
     {
         // load the stage (normally stdlight.lua)
-        const char* result;
+        nString result;
         this->refScriptServer->RunScript(this->GetStageScript(), result);
 
         // load the object to look at
