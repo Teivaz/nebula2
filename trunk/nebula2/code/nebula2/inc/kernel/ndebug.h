c4f247f54976b52646bedaa23c5188005c6ed873
badsmurf <badsmurf@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-01-21 11:36:37 +0000

    Added n_verify2 to match n_assert2

--- trunk/nebula2/code/nebula2/inc/kernel/ndebug.h
+++ trunk/nebula2/code/nebula2/inc/kernel/ndebug.h
@@ -13,6 +13,7 @@
 #ifdef __NEBULA_NO_ASSERT__
 #define n_assert(exp)
 #define n_verify(exp) (exp)
+#define n_verify2(exp,msg) (exp)
 #define n_assert2(exp,msg)
 #else
 #define n_assert(exp) { if (!(exp)) n_barf(#exp,__FILE__,__LINE__); }
@@ -20,6 +21,7 @@
 
 // an assert with a verbose hint from the programmer
 #define n_assert2(exp,msg) { if (!(exp)) n_barf2(#exp,msg,__FILE__,__LINE__); }
+#define n_verify2(exp,msg) { if (!(exp)) n_barf2(#exp,msg,__FILE__,__LINE__); }
 
 // dx9 specific: check HRESULT and display DX9 specific message box
 #define n_dxtrace(hr,msg) { if (FAILED(hr)) DXTrace(__FILE__,__LINE__,hr,msg,true); }
