3db2b6efc73c6ce6651245198f8253a73838c6f5
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-02-16 14:33:12 +0000

    - module for converting nebula object to 3dsmax custom attributes

---
+++ nebula2/code/contrib/nmax/inc/expose/nmaxscriptbuilder.h
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+//  nmaxscriptbuilder.h
+//
+//  (C)2004 Kim, Hyoun Woo.
+//------------------------------------------------------------------------------
+#ifndef N_MAXSCRIPTBUILDER_H
+#define N_MAXSCRIPTBUILDER_H
+
+//------------------------------------------------------------------------------
+/**
+    @class
+*/
+
+class nMaxScriptBuilder
+{
+public:
+    static char* BuildMaxScriptFromNebulaObject(const char* objPath);
+
+
+};
+//------------------------------------------------------------------------------
+#endif
+
