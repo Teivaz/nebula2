f8c3adc3edeb6829a0cdfb59158a9344d65c9052
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-02-17 22:43:36 +0000

    - redesigned to make it more clear

--- nebula2/code/contrib/nmax/inc/export2/nmaxcontroller.h
+++ nebula2/code/contrib/nmax/inc/export2/nmaxcontroller.h
@@ -1,16 +1,62 @@
+//-----------------------------------------------------------------------------
+//  nmaxcontrol.h
+//
+//  (C)2004 Kim, Hyoun Woo
+//-----------------------------------------------------------------------------
 #ifndef N_MAXCONTROLLER_H
 #define N_MAXCONTROLLER_H
 //-----------------------------------------------------------------------------
-/**
-    @class nMaxController
-    @ingroup
-
-    @brief A wrapper class for 3dsmax controller.
-
-*/
+#include "export2/nmaxnode.h"
+#include "util/narray.h"
 
 //-----------------------------------------------------------------------------
-class nMaxController
+/**
+*/
+enum nMaxControlType 
+{
+    nMaxPos,        /// Position Controller
+    nMaxRot,        /// Rotation Controller
+    nMaxScale,      /// Scale Controller
+    nMaxFloat,      /// Float Controller
+    nMaxPoint3,     /// Point3 Controller
+    nMaxTM,         /// Used for sampling the node transformation matrix.  
+    nMaxEulerX,     /// Euler X controller
+    nMaxEulerY,     /// Euler Y controller
+    nMaxEulerZ,     /// Euler Z controller
+    nMaxPoint4,     /// Point4 based controllers - This is available from 3ds max 6.
+
+};
+
+//-----------------------------------------------------------------------------
+/**
+    @class nMaxSampleKey
+    @ingroup NebulaMaxExport2Contrib
+
+    @brief A class for sampled key of a control.
+
+    @note
+        pos, rot, scale members are decomposed for nMaxTM for easy to use.
+*/
+struct nMaxSampleKey 
+{
+    Matrix3 tm;    // use it for nMaxTM
+    Point3  pos;   // use it for nMaxPoint3 
+    Quat    rot;   // use if for nMaxRot
+    Point3  scale; // use it for nMaxScale
+    float   fVal;  // use it for nMaxFloat
+
+    float time;
+};
+
+//-----------------------------------------------------------------------------
+/**
+    @class nMaxControl
+    @ingroup NebulaMaxExport2Contrib
+
+    @brief A class for getting keys from controllers.
+
+*/
+class nMaxControl
 {
 public:
 	enum Type 
@@ -29,15 +75,20 @@ public:
 		LinearPosition,
 		LinearRotation,
 		LinearScale,
+        EulerRotation,
         Unknown,
 	};
 
-    nMaxController();
-    virtual ~nMaxController();
+    nMaxControl();
+    virtual ~nMaxControl();
 
-    //virtual void Export(INode *inode);
+    static void GetSampledKey(INode* node, nArray<nMaxSampleKey> & sampleKeyArray, 
+                           int sampleRate, nMaxControlType type);
 
-	Type GetType(Control *control);
+    static void GetSampledKey(Control* control, nArray<nMaxSampleKey> & sampleKeyArray, 
+        int sampleRate, nMaxControlType type);
+
+	static Type GetType(Control *control);
 
 };
 //-----------------------------------------------------------------------------
--- nebula2/code/contrib/nmax/inc/export2/nmaxprscontroller.h
+++ nebula2/code/contrib/nmax/inc/export2/nmaxprscontroller.h
@@ -1,5 +1,10 @@
-#ifndef N_MAXPRSCONTROLLER_H
-#define N_MAXPRSCONTROLLER_H
+//-----------------------------------------------------------------------------
+//  nmaxprscontroller.h
+//
+//  (C)2004 Kim, Hyoun Woo
+//-----------------------------------------------------------------------------
+#ifndef N_MAXTRANSFROMANIMATOR_H
+#define N_MAXTRANSFROMANIMATOR_H
 //-----------------------------------------------------------------------------
 /**
     @class nMaxPRSController
@@ -8,28 +13,46 @@
     @brief A class for handling Position/Rotation/Scale(PRS)
 
 */
+#include "export2/nmaxnode.h"
 
-class nMaxController;
+
 class nTransformAnimator;
 
 //-----------------------------------------------------------------------------
-class nMaxPRSController : public nMaxController
+class nMaxTransformAnimator : public nMaxNode
 {
 public:
-    nMaxPRSController();
-    virtual ~nMaxPRSController();
+    nMaxTransformAnimator();
+    virtual ~nMaxTransformAnimator();
 
-    virtual void Export(INode* inode);
+    virtual void Export(INode* inode, Control *control);
 
 protected:
-	virtual void ExportPosition(Control *control, nTransformAnimator* animator);
-	virtual void ExportRotation(Control *control, nTransformAnimator* animator);
-	virtual void ExportScale(Control *control, nTransformAnimator* animator);
+    // @name position
+    // @{
+    virtual void ExportPosition(Control *control, nTransformAnimator* animator);
 
-	virtual void ExportTCBPosition();
-	virtual void ExportHybridPosition();
-	virtual void ExportLinearPosition();
-    virtual void ExportDefaultPosition(INode *inode);
+	virtual void ExportTCBPosition(IKeyControl* ikc, int numKeys);
+	virtual void ExportHybridPosition(IKeyControl* ikc, int numKeys);
+	virtual void ExportLinearPosition(IKeyControl* ikc, int numKeys);
+    virtual void ExportSampledKeyPosition(int numKeys, nTransformAnimator* animator);
+    // @}
+
+    // @name rotation
+    // @{
+    virtual void ExportRotation(Control *control, nTransformAnimator* animator);
+
+    virtual void ExportTCBRotation(IKeyControl* ikc, int numKeys);
+    virtual void ExportHybridRotation(IKeyControl* ikc, int numKeys);
+    virtual void ExportLinearRotation(IKeyControl* ikc, int numKeys);
+    virtual void ExportEulerRotation(Control* control, int numKeys);
+    virtual void ExportSampledKeyRotation(int numKeys, nTransformAnimator* animator);
+    // @}
+
+    // @name scale
+    // @{
+    virtual void ExportScale(Control *control, nTransformAnimator* animator);
+    // @}    
 	
     bool HasSampledKeys(Control *control);
 
