7934c7d5d7625a151f758619b838e341ab88fb4a
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-02-18 02:40:27 +0000

    modified included header filename to make it compatible to filename changes

--- nebula2/code/contrib/nmax/src/export2/nmaxmaterial_std.cc
+++ nebula2/code/contrib/nmax/src/export2/nmaxmaterial_std.cc
@@ -13,9 +13,7 @@
 
 #include "pluginlibs/nmaxdlg.h"
 #include "pluginlibs/nmaxlogdlg.h"
-#include "export2/nmaxcontroller.h"
-//#include "export2/nmaxpoint3controller.h"
-//#include "export2/nmaxfloatcontroller.h"
+#include "export2/nmaxcontrol.h"
 
 #include "scene/nshapenode.h"
 
