520740e1275cdec67f3c1059435cef0bbf9aface
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-02-22 01:40:40 +0000

    This is probably a working nAppLauncher for Linux. I didn't test it, but it is a modified copy of code that I have elsewhere that does work (and is suitably licensed to copy).

--- trunk/nebula2/code/nebula2/src/tools/napplauncher.cc
+++ trunk/nebula2/code/nebula2/src/tools/napplauncher.cc
@@ -4,8 +4,14 @@
 //------------------------------------------------------------------------------
 #include "tools/napplauncher.h"
 #include "kernel/nfileserver2.h"
+#ifdef __WIN32__
 #include <windows.h>
 #include <shellapi.h>
+#elif defined(__LINUX__)
+#include <limits.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#endif
 
 //------------------------------------------------------------------------------
 /**
@@ -34,6 +40,7 @@ nAppLauncher::LaunchWait() const
     n_assert(!this->exec.IsEmpty());
     n_assert(this->kernelServer);
 
+#ifdef __WIN32__
     STARTUPINFO startupInfo = { sizeof(STARTUPINFO), 0 };
     PROCESS_INFORMATION processInfo = { 0 };
 
@@ -71,6 +78,11 @@ nAppLauncher::LaunchWait() const
     // cleanup
     CloseHandle(processInfo.hProcess);
     CloseHandle(processInfo.hThread);
+#elif defined(__LINUX__)
+    return this->LaunchHelper(true);
+#else
+    return false;
+#endif
 
     return true;
 }
@@ -112,6 +124,7 @@ nAppLauncher::Launch() const
         dirMangled  = nFileServer2::Instance()->ManglePath(this->dir.Get());
         dirPtr = dirMangled.Get();
     }
+#ifdef __WIN32__
     const char* argPtr = 0;
     if (!this->args.IsEmpty())
     {
@@ -124,6 +137,83 @@ nAppLauncher::Launch() const
                                     dirPtr,                 // lpDirectory
                                     SW_SHOW);               // nShowCmd
     return ((int)retval > 32);
+#elif defined(__LINUX__)
+    return this->LaunchHelper(false);
+#else
+    return false;
+#endif
 }
 
+#ifdef __LINUX__
+bool
+nAppLauncher::LaunchHelper(bool waitForChild) const
+{
+    // mangle paths
+    nString execMangled = nFileServer2::Instance()->ManglePath(this->exec.Get());
+    nString dirMangled  = nFileServer2::Instance()->ManglePath(this->dir.Get());
+
+    nString appCmdLine;
+    appCmdLine.Format("%s/%s", dirMangled.Get(), execMangled.Get());
+    const char *execCmd = appCmdLine.Get();
+
+    // fork this process, and replace it with intended target
+    int pid = fork();
+    if (pid == 0) {
+        // +2 -> 1 for the command name, 1 for the NULL
+        char  *argv[ARG_MAX + 2];
+        argv[0] = (char*)execMangled.Get();
+        int argc = 1;
+
+        // tokenize command line
+        // XXX: This cast is bad.
+        char* ptr = (char*)this->args.Get();
+        char* end = ptr + strlen(ptr);
+
+        while ((argc < ARG_MAX) && (ptr < end))
+        {
+            char* c;
+            while ((' ' == *ptr) || ('\t' == *ptr))
+                ptr++;
+            if (('"' == *ptr) && (c = strchr(++ptr, '"')))
+            {
+                *c++ = 0;
+                argv[argc++] = ptr;
+                ptr = c;
+            }
+            else if ((c = strpbrk(ptr, N_WHITESPACE)))
+            {
+                *c++ = 0;
+                argv[argc++] = ptr;
+                ptr = c;
+            }
+            else
+            {
+                argv[argc++] = ptr;
+                break;
+            }
+        }
+        argv[argc] = NULL;
+
+        // we have the args, so now we can exec the new process
+        execv(execCmd, argv);
+        exit(-1);
+    } else if (pid > 0) {
+        // Wait for the child...
+        int status = 0;
+        if (waitForChild)
+        {
+            waitpid(pid, &status, 0);
+        }
+        else
+        {
+            waitpid(pid, &status, WNOHANG);
+        }
+    } else {
+        // Failed to fork
+        return false;
+    }
+
+    return true;
+}
+#endif
 
