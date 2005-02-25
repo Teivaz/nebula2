0c4139c0b2a664283b501e4e0532abf76aec82aa
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-02-25 00:34:53 +0000

    updated document

--- nebula2/code/contrib/nmax/inc/export2/nmaxnode.h
+++ nebula2/code/contrib/nmax/inc/export2/nmaxnode.h
@@ -13,6 +13,10 @@
     @brief Base class of Nebula max object class which correspondes to each of
            the 3DS Max object.
 
+    You can extend the exporter with creating a new class which derives this
+    nMaxNode base class to export a 3DS Max object which is not supported by
+    the exporter.
+
 */
 class nSceneNode;
 
