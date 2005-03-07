2b934fee72ede7b479fe40a78ff36ebe46a8a650
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-03-07 03:11:01 +0000

    Remove whitespace @ EOL.

--- nebula2/code/nebula2/inc/kernel/ncmdprotonative.h
+++ nebula2/code/nebula2/inc/kernel/ncmdprotonative.h
@@ -9,26 +9,26 @@
          script commands.
 
   (c) 2003 Vadim Macagon
-  
+
   nCmdProtoNative is licensed under the terms of the Nebula License
 */
 #include "kernel/ncmdproto.h"
 
 //--------------------------------------------------------------------
-class nCmdProtoNative : public nCmdProto 
+class nCmdProtoNative : public nCmdProto
 {
 public:
     /// constructor
     nCmdProtoNative(const char *_proto_def, nFourCC _id, void (*)(void *, nCmd *));
     /// copy constructor
     nCmdProtoNative(const nCmdProtoNative& rhs);
-    
+
     bool Dispatch(void *, nCmd *);
-    
+
 private:
     /// pointer to C style command handler
     void (*cmd_proc)(void *, nCmd *);
 };
 //--------------------------------------------------------------------
-#endif    
+#endif
 
