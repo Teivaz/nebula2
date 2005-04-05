bec30ada3a2a4cacc2a440f97f494319bbcca6ae
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-04-05 06:32:36 +0000

    Define n_dxtrace() when asserts are off so that code will compile. Spotted by Jindrich Rohlik.

--- trunk/nebula2/code/nebula2/inc/kernel/ndebug.h
+++ trunk/nebula2/code/nebula2/inc/kernel/ndebug.h
@@ -15,6 +15,7 @@
 #define n_verify(exp) (exp)
 #define n_verify2(exp,msg) (exp)
 #define n_assert2(exp,msg)
+#define n_dxtrace(hr,msg)
 #else
 #define n_assert(exp) { if (!(exp)) n_barf(#exp,__FILE__,__LINE__); }
 #define n_verify(exp) { if (!(exp)) n_barf(#exp,__FILE__,__LINE__); }
