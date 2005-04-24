23c0777183d57a68497cb619daa9e6d2f1251230
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-04-24 05:46:59 +0000

    helper file to easily run wxnebula.py

---
+++ nebula2/code/contrib/wxnebula/bin/wxnebula.bat
@@ -0,0 +1,6 @@
+
+REM batch file to help running wxnebula.py
+REM copy this to '$nebula2/bin/win32' or '$nebula2/bin/win32d' and run 
+
+npyviewer -projdir ../../ -startup ../../code/contrib/wxnebula/bin/wxnebula.py
+
