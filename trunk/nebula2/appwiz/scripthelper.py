2fad6654f53187290bb2c952757ba054808e0ac7
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-06-11 05:07:28 +0000

    Initial submit of the Nebula2 application wizard python module files.

---
+++ trunk/nebula2/appwiz/scripthelper.py
@@ -0,0 +1,108 @@
+#----------------------------------------------------------------------
+#  scripthelper.py  
+#
+#  (C)2005 Kim, Hyoun woo
+#----------------------------------------------------------------------
+
+STR_PYTHON_REGISTER_PAKCAGES = '''
+//----------------------------------------------------------------------------
+/**     
+    Package registration hook.
+*/
+void
+nPythonRegisterPackages(nKernelServer * kernelServer)
+{
+    kernelServer->AddPackage(nnebula);
+    kernelServer->AddPackage(ndirect3d9);
+    kernelServer->AddPackage(ndinput8);
+    kernelServer->AddPackage(ndsaudioserver3);
+    kernelServer->AddPackage(ndshow);
+    kernelServer->AddPackage(ngui);
+    kernelServer->AddPackage(napplication);
+    kernelServer->AddPackage(npythonserver);
+    kernelServer->AddPackage(%(targetName)s);
+}
+'''
+
+STR_ADD_PACKAGES = '''
+    kernelServer.AddPackage(nnebula);
+    kernelServer.AddPackage(ndirect3d9);
+    kernelServer.AddPackage(ndinput8);
+    kernelServer.AddPackage(ndsaudioserver3);
+    kernelServer.AddPackage(ndshow);
+    kernelServer.AddPackage(ngui);
+    kernelServer.AddPackage(napplication);
+    %(scriptServerAddPackage)s
+    kernelServer.AddPackage(%(targetName)s);\
+'''
+
+#----------------------------------------------------------------------
+#  
+#----------------------------------------------------------------------
+class ScriptHelper:
+    def __init__(self, classname, targetname, scriptserver):
+
+        className = classname
+        targetName = targetname
+        # script server name
+        scriptServerName = scriptserver
+
+        packageScriptServerName = ''
+        self.scriptServerPackage = ''
+        self.bldScriptServerName = ''
+        # we dont have to add ntclserver to target (it causes error when execute update.py)
+        if 'ntclserver' == scriptServerName:
+            packageScriptServerName = ''
+            self.scriptServerPackage = ''
+        else:
+            self.bldScriptServerName = scriptServerName
+            packageScriptServerName = scriptServerName
+            self.scriptServerPackage = 'nNebulaUsePackage(' + packageScriptServerName + ');'
+
+        self.pythonRegPackage = ''
+        self.addPackageDef = ''
+
+        if 'npythonserver' == scriptServerName:
+            self.pythonRegPackage = STR_PYTHON_REGISTER_PAKCAGES % {'classNameL' : className.lower()}
+            self.addPackageDef = 'nPythonRegisterPackages (&kernelServer);'
+        else:
+            self.pythonRegPackage = '' 
+            scriptServerAddPackage = ''
+            if 'ntclserver' == scriptServerName:
+                scriptServerAddPackage = ''
+            else:
+                tmp = 'kernelServer.AddPackage(%(scriptServerName)s);'
+                scriptServerAddPackage = tmp % { 'scriptServerName' : scriptServerName }
+
+            self.addPackageDef = STR_ADD_PACKAGES % { 'classNameL' : className.lower(), 
+                                                      'scriptServerAddPackage' : scriptServerAddPackage,
+                                                      'targetName' : targetName.lower()
+                                                    }
+
+   #----------------------------------------------------------------------
+   #  
+   #----------------------------------------------------------------------
+    def GetScriptServerPackage(self):
+        return self.scriptServerPackage
+
+   #----------------------------------------------------------------------
+   #  
+   #----------------------------------------------------------------------
+    def GetBldScriptServerName(self):
+        return self.bldScriptServerName 
+
+   #----------------------------------------------------------------------
+   #  
+   #----------------------------------------------------------------------
+    def GetPythonRegisterPackage(self):
+        return self.pythonRegPackage
+
+   #----------------------------------------------------------------------
+   #  
+   #----------------------------------------------------------------------
+    def GetAddPackageDef(self):
+        return self.addPackageDef
+
+#----------------------------------------------------------------------
+# EOF 
+#----------------------------------------------------------------------
