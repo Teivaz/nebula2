ef4ef09d5b398fc6770fef92ba5811d7ff4aca95
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-06-19 00:28:03 +0000

    Fix wxnebula to work again.  * Correctly check the event loop that we create by calling methods on    that event loop object rather than on the application.  * Store the application object on the main frame so that it can    handle closing the window correctly.  * Remove a return that I'd added earlier that stopped node selection    in the NOH tree from working.

--- nebula2/code/contrib/wxnebula/wxnebula/app.py
+++ nebula2/code/contrib/wxnebula/wxnebula/app.py
@@ -6,14 +6,18 @@ from wxnebula.controls import MainWindowFrame
 class NebulaWXApp(wx.App):
     def OnInit(self):
         frame = MainWindowFrame()
+        frame.SetApplication(self)
         frame.Show(True)
         self.SetTopWindow(frame)
         self.keepGoing = True
+        # Set up our very own event loop...
+        self.eventLoop = wx.EventLoop()
+        wx.EventLoop.SetActive(self.eventLoop)
         return True
 
     def Trigger(self):
-        while self.Pending():
-            self.Dispatch()
+        while self.eventLoop.Pending():
+            self.eventLoop.Dispatch()
         self.ProcessIdle()
         if self.keepGoing == False:
             nebula.exit()
