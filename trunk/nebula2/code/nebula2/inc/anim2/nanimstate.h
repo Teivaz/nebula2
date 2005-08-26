f8c3518335b57a39cc96619df11292a6789bdf05
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-08-26 16:04:27 +0000

    - August 2005 merge with RL - Added some new member functions. - Moved Sample() member function to the implementation file.

--- trunk/nebula2/code/nebula2/inc/anim2/nanimstate.h
+++ trunk/nebula2/code/nebula2/inc/anim2/nanimstate.h
@@ -20,6 +20,7 @@
 #include "anim2/nanimclip.h"
 #include "variable/nvariablecontext.h"
 #include "mathlib/quaternion.h"
+#include "anim2/nanimeventhandler.h"
 
 //------------------------------------------------------------------------------
 class nAnimState
@@ -51,12 +52,22 @@ public:
     nAnimClip& GetClipAt(int index) const;
     /// sample weighted values at a given time from nAnimation object
     bool Sample(float time, nAnimation* animSource, nVariableContext* varContext, vector4* keyArray, vector4* scratchKeyArray, int keyArraySize);
+    /// emit animation events for a given time range
+    void EmitAnimEvents(float fromTime, float toTime, nAnimation* animSource, nAnimEventHandler* handler);
 
 private:
+    /// begin defining blended animation events
+    void BeginEmitEvents();
+    /// add a blended animation event
+    void AddEmitEvent(const nAnimEventTrack& track, const nAnimEvent& event, float weight);
+    /// finish defining blended anim events, emit the events
+    void EndEmitEvents(nAnimEventHandler* handler);
+        
     nArray<nAnimClip> clipArray;
     nString name;
     int animGroupIndex;
     float fadeInTime;
+    nArray<nAnimEventTrack> outAnimEventTracks;
 };
 
 //------------------------------------------------------------------------------
@@ -184,134 +195,6 @@ nAnimClip&
 nAnimState::GetClipAt(int index) const
 {
     return this->clipArray[index];
-}
-
-//------------------------------------------------------------------------------
-/**
-    Sample the complete animation state at a given time into the
-    provided float4 array. The size of the provided key array must be
-    equal to the number of curves in any anim clip in the state (all clips
-    must have the same number of curves).
-
-    @param  time            the time at which to sample
-    @param  animSource      the nAnimation object which provides the anim data
-    @param  varContext      a variable context containing the clip weights
-    @param  keyArray        pointer to a float4 array which will be filled with 
-                            the sampled values (one per curve)
-    @param  keyArraySize    number of elements in the key array, must be identical 
-                            to the number of curves in any anim clip
-    @return                 true, if the returned keys are valid (false if all
-                            clip weights are zero)
-*/
-inline
-bool
-nAnimState::Sample(float time, 
-                   nAnimation* animSource, 
-                   nVariableContext* varContext, 
-                   vector4* keyArray, 
-                   vector4* scratchKeyArray,
-                   int keyArraySize)
-{
-    n_assert(animSource);
-    n_assert(keyArray);
-    n_assert(keyArraySize >= this->GetClipAt(0).GetNumCurves());
-    n_assert(varContext);
-
-    // first, read the clip weights from the variable context and
-    // sum them up, so that we can normalize the clip weights later
-    int clipIndex;
-    int numClips = this->GetNumClips();
-    float weightSum = 0.0f;
-    for (clipIndex = 0; clipIndex < numClips; clipIndex++)
-    {
-        nAnimClip& clip = this->GetClipAt(clipIndex);
-        float weight = 0.0f;
-        nVariable* var = varContext->GetVariable(clip.GetWeightChannelHandle());
-        if (var)
-        {
-            weight = var->GetFloat();
-        }
-        clip.SetWeight(weight);
-        weightSum += weight;
-    }
-    if (weightSum > 0.0f)
-    {
-        // some static helper objects
-        static quaternion quatCurrent;
-        static quaternion quatAccum;
-        static quaternion quatSlerp;
-
-        // for each clip...
-        bool firstIter = true;
-        float weightAccum = 0.0f;
-        for (clipIndex = 0; clipIndex < numClips; clipIndex++)
-        {
-            nAnimClip& clip  = this->GetClipAt(clipIndex);
-            float clipWeight = clip.GetWeight() / weightSum;
-
-            // only process clip if its weight is greater 0
-            if (clipWeight > 0.0f)
-            {
-                // scale weightAccum so that 1 == (weightAccum + weight)
-                const float scaledWeight = clipWeight / (weightAccum + clipWeight);
-
-                // for each curve in the clip...
-                int numCurves = clip.GetNumCurves();
-                int firstCurveIndex = clip.GetFirstCurveIndex();
-
-                // obtain sampled curve value for the clip's anim curve range
-                animSource->SampleCurves(time, this->animGroupIndex, firstCurveIndex, numCurves, scratchKeyArray);
-
-                int curveIndex;
-                for (curveIndex = 0; curveIndex < numCurves; curveIndex++)
-                {
-                    int absCurveIndex = firstCurveIndex + curveIndex;
-                    vector4& curArrayKey  = keyArray[curveIndex];
-                    const vector4& curSampleKey = scratchKeyArray[curveIndex];
-
-                    // perform weighted blending
-                    nAnimation::Curve& animCurve = animSource->GetGroupAt(this->animGroupIndex).GetCurveAt(absCurveIndex);
-                    if (animCurve.GetIpolType() == nAnimation::Curve::Quat)
-                    {
-                        // for quaternions, blending is a bit tricky...
-                        if (firstIter)
-                        {
-                            // first time init of accumulators
-                            curArrayKey = curSampleKey;
-                        }
-                        else
-                        {
-                            // FIXME: all those vector4/quaternion transfers are inefficient
-                            quatCurrent.set(curSampleKey.x, curSampleKey.y, curSampleKey.z, curSampleKey.w);
-                            quatAccum.set(curArrayKey.x, curArrayKey.y, curArrayKey.z, curArrayKey.w);
-                            quatSlerp.slerp(quatAccum, quatCurrent, scaledWeight);
-                            curArrayKey.set(quatSlerp.x, quatSlerp.y, quatSlerp.z, quatSlerp.w);
-                        }
-                    }
-                    else
-                    {
-                        // do normal linear blending
-                        if (firstIter)
-                        {
-                            curArrayKey = curSampleKey * clipWeight;
-                        }
-                        else
-                        {
-                            curArrayKey += curSampleKey * clipWeight;
-                        }
-                    }
-                }
-                firstIter = false;
-                weightAccum += clipWeight;
-            }
-        }
-        return true;
-    }
-    else
-    {
-        // sum of weight is zero
-        return false;
-    }
 }
 
 //------------------------------------------------------------------------------
