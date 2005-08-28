63e33d8f73a81f2734bc08a687d62b6071812c09
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-08-28 04:01:26 +0000

    New files from this merge.

---
+++ trunk/nebula2/code/nebula2/src/gui/nguislide_main.cc
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+//  nguislide_main.cc
+//  (C) 2005 RadonLabs GmbH
+//------------------------------------------------------------------------------
+
+#include "gui/nguislide.h"
+#include "gui/nguiresource.h"
+
+nNebulaClass(nGuiSlide, "nguilabel");
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiSlide::nGuiSlide()
+{
+    //empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+nGuiSlide::~nGuiSlide()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+nGuiSlide::SetRenderColor(vector4 color)
+{
+    if(!this->defaultBrush.GetName().IsEmpty())
+    {
+        nGuiResource* res = this->defaultBrush.GetGuiResource();
+        res->SetColor(color);
+    }
+}
