0f0e697e61dfe628902402af575feae4daaa972f
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-09-17 11:27:01 +0000

    Move this code back now that we have nInputServer::Open() to make this cleaner.

--- trunk/nebula2/code/nebula2/src/tools/nviewerapp.cc
+++ trunk/nebula2/code/nebula2/src/tools/nviewerapp.cc
@@ -52,6 +52,7 @@ nViewerApp::Open()
     // initialize Nebula servers
     this->refScriptServer   = (nScriptServer*)    kernelServer->New(this->GetScriptServerClass().Get(), "/sys/servers/script");
     this->refGfxServer      = (nGfxServer2*)      kernelServer->New(this->GetGfxServerClass().Get(), "/sys/servers/gfx");
+    this->refInputServer    = (nInputServer*)     kernelServer->New("ndi8server", "/sys/servers/input");
     this->refConServer      = (nConServer*)       kernelServer->New("nconserver", "/sys/servers/console");
     this->refResourceServer = (nResourceServer*)  kernelServer->New("nresourceserver", "/sys/servers/resource");
     this->refSceneServer    = (nSceneServer*)     kernelServer->New(this->GetSceneServerClass().Get(), "/sys/servers/scene");
@@ -131,9 +132,6 @@ nViewerApp::Open()
     this->refVideoServer->Open();
 
     // define the input mapping
-    // late initialization of input server, because it relies on 
-    // refGfxServer->OpenDisplay having been called
-    this->refInputServer    = (nInputServer*)     kernelServer->New("ndi8server", "/sys/servers/input");
     this->refInputServer->Open();
     this->DefineInputMapping();
 
