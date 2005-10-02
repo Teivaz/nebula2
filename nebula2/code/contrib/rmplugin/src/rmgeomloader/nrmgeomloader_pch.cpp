65a66cc3775af291b2a92fcb3cf2debb85eb6493
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2005-10-02 01:12:11 +0000

    Initial release of Nebula2 RenderMonkey geometry loader plugin

---
+++ nebula2/code/contrib/rmplugin/src/rmgeomloader/nrmgeomloader_pch.cpp
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+//  nrmgeomloader_pch.cc
+//
+//  Precompiled Header for the geometry loader plug-in.
+//
+//  (C) 2005 Kim, Hyoun Woo.
+//-----------------------------------------------------------------------------
+#include "rmgeomloader/nrmgeomloader_pch.h"
