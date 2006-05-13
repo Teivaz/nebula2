fa88aa0a3e7e23e7f5e8a1c44ce1230ec86e0854
brucem <brucem@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2006-05-13 19:11:35 +0000

    Removing unmaintained things.

--- nebula2/code/contrib/bombsquadbruce/data/scripts/strings/deutsch.lua
+++
@@ -1,9 +0,0 @@
----------------------
--- Menu labels
----------------------
-items = guiServer.modes.mainMenu.items
-items.continue.label = 'Spiel fortsetzen'
-items.play.label = 'Neues Spiel'
-items.options.label = 'Einstellungen'
-items.quit.label = 'Spiel verlassen'
-
--- nebula2/code/contrib/bombsquadbruce/data/scripts/strings/english.lua
+++
@@ -1,9 +0,0 @@
----------------------
--- Menu labels
----------------------
-items = guiServer.modes.mainMenu.items
-items.continue.label = 'Continue game'
-items.play.label = 'New Game'
-items.options.label = 'Configure'
-items.quit.label = 'Quit'
-
