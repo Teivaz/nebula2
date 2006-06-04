40a5f6789b9a0923e6433c54c3ee2a0353b937fa
johndotsmith <johndotsmith@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2006-06-04 13:21:27 +0000

    update loki template library to 0.1.4

--- trunk/nebula2/code/nebula2/inc/loki/NullType.h
+++ trunk/nebula2/code/nebula2/inc/loki/NullType.h
@@ -1,26 +1,46 @@
-////////////////////////////////
-// Generated header: NullType.h
-// Forwards to the appropriate code
-// that works on the detected compiler
-// Generated on Mon Sep 30 23:14:48 2002
-////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// The Loki Library
+// Copyright (c) 2001 by Andrei Alexandrescu
+// This code accompanies the book:
+// Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
+//     Patterns Applied". Copyright (c) 2001. Addison-Wesley.
+// Permission to use, copy, modify, distribute and sell this software for any 
+//     purpose is hereby granted without fee, provided that the above copyright 
+//     notice appear in all copies and that both that copyright notice and this 
+//     permission notice appear in supporting documentation.
+// The author or Addison-Wesley Longman make no representations about the 
+//     suitability of this software for any purpose. It is provided "as is" 
+//     without express or implied warranty.
+////////////////////////////////////////////////////////////////////////////////
 
-#ifdef LOKI_USE_REFERENCE
-#	include "Reference/NullType.h"
-#else
-#	if (__INTEL_COMPILER)
-#		include "Reference/NullType.h"
-#	elif (__MWERKS__)
-#		include "Reference/NullType.h"
-#	elif (__BORLANDC__ >= 0x560)
-#		include "Borland/NullType.h"
-#	elif (_MSC_VER >= 1301)
-#		include "Reference/NullType.h"
-#	elif (_MSC_VER >= 1300)
-#		include "MSVC/1300/NullType.h"
-#	elif (_MSC_VER >= 1200)
-#		include "MSVC/1200/NullType.h"
-#	else
-#		include "Reference/NullType.h"
-#	endif
-#endif
+// Last update: November 22, 2001
+
+#ifndef LOKI_NULLTYPE_INC_
+#define LOKI_NULLTYPE_INC_
+
+// $Header: /cvsroot/loki-lib/loki/include/loki/NullType.h,v 1.3 2006/01/16 19:05:09 rich_sposato Exp $
+
+namespace Loki
+{
+////////////////////////////////////////////////////////////////////////////////
+// class NullType
+// Used as a placeholder for "no type here"
+// Useful as an end marker in typelists 
+////////////////////////////////////////////////////////////////////////////////
+
+    class NullType {};
+    
+}   // namespace Loki
+
+////////////////////////////////////////////////////////////////////////////////
+// Change log:
+// June 20, 2001: ported by Nick Thurn to gcc 2.95.3. Kudos, Nick!!!
+// November 22, 2001: minor change to support porting to boost
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // NULLTYPE_INC_
+
+// $Log: NullType.h,v $
+// Revision 1.3  2006/01/16 19:05:09  rich_sposato
+// Added cvs keywords.
+//
