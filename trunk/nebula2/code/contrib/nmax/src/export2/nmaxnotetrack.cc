06260a125bed4f9ab01e02c82993662629a6bbe7
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2006-07-11 13:56:37 +0000

    Modified to recognize loop type from Note Track in the animation.

--- trunk/nebula2/code/contrib/nmax/src/export2/nmaxnotetrack.cc
+++ trunk/nebula2/code/contrib/nmax/src/export2/nmaxnotetrack.cc
@@ -64,6 +64,30 @@ nMaxNoteTrack::~nMaxNoteTrack()
 
 //---------------------------------------------------------------------------
 /**
+    Extract note name.
+    e.g) get a word 'walk' with the given string, "walk : repeat".
+*/
+nString nMaxNoteTrack::ExtractNoteName(nString& rhs)
+{
+    nString tmp = rhs;
+    tmp.TrimRight(":");
+    return nMaxUtil::CorrectName(tmp);
+}
+
+//---------------------------------------------------------------------------
+/**
+    Extract loop type.
+    e.g) get a word 'repeat' with the given string, "walk : repeat".
+*/
+nString nMaxNoteTrack::ExtractLoopType(nString& rhs)
+{
+    nString tmp = rhs;
+    tmp.TrimLeft(":");
+    return nMaxUtil::CorrectName(tmp);
+}
+
+//---------------------------------------------------------------------------
+/**
     Collect note key from the given node and creates animation states.
 
     A note key is used for representing animation state.
@@ -93,17 +117,27 @@ void nMaxNoteTrack::GetAnimState(INode* inode)
             if (!note.IsEmpty())
             {
                 int duration = 0;
-                nString strNote, strDuration;
+                nString strNote, strNoteName, strDuration, strLoopType;
 
                 int idx = note.FindCharIndex('\n', 0);
-                if (idx > 0)
+                if (idx > 0) // we have both note key and duration for the property
                 {
-                    strNote    = nMaxUtil::CorrectName(note.ExtractRange(0, idx));
+                    strNote = nMaxUtil::CorrectName(note.ExtractRange(0, idx));
+                    if (strNote.ContainsCharFromSet(":"))
+                    {
+                        strNoteName = this->ExtractNoteName(strNote);
+                        strLoopType = this->ExtractLoopType(strNote);
+                    }
+                    else
+                    {
+                        strNoteName = strNote;
+                    }
+
                     int durLen = note.Length() - idx - 1;
                     if (durLen > 0) 
                     {
                         strDuration = nMaxUtil::CorrectName(note.ExtractRange(idx+1, durLen));
-                        duration = atoi(strDuration.Get());
+                        duration = strDuration.AsInt();//duration = atoi(strDuration.Get());
                     }
 
                     if (duration <= 0)
@@ -111,19 +145,45 @@ void nMaxNoteTrack::GetAnimState(INode* inode)
                         duration = nextFrame - frame;
                     }
                 }
-                else
+                else // we only have note key name
                 {
                     strNote = nMaxUtil::CorrectName(note);
+                    if (strNote.ContainsCharFromSet(":"))
+                    {
+                        strNoteName = this->ExtractNoteName(strNote);
+                        strLoopType = this->ExtractLoopType(strNote);
+                    }
+                    else
+                    {
+                        strNoteName = strNote;
+                    }
+
                     duration = nextFrame - frame;
                 }
 
                 // add animation state.
                 nMaxAnimState animState;
 
-                animState.name       = strNote;
+                animState.name       = strNoteName;
                 animState.firstFrame = frame;
                 animState.duration   = duration;
                 animState.fadeInTime = 0.0f;
+
+                strLoopType.ToLower();
+                if (strLoopType == "repeat")
+                {
+                    animState.loopType = nAnimBuilder::Group::REPEAT;
+                }
+                else
+                if (strLoopType == "clamp")
+                {
+                    animState.loopType = nAnimBuilder::Group::CLAMP;
+                }
+                else
+                {
+                    // unknown loop type. just specify it to repeat type.
+                    animState.loopType = nAnimBuilder::Group::REPEAT;
+                }
 
                 this->stateArray.Append(animState);
             }
