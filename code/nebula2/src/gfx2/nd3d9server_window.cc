a67c6f6ccf68bd22985fe1be5ac8124c0323874f
johndotsmith <johndotsmith@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2006-07-23 05:42:15 +0000

    cleanup nebula2 bldfiles, step1: remove old files.

--- code/nebula2/src/gfx2/nd3d9server_window.cc
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------
-//  nd3d9server_window.cc
-//  (C) 2003 RadonLabs GmbH
-//------------------------------------------------------------------------------
-#include "gfx2/nd3d9server.h"
-#include "input/ninputserver.h"
-
-LONG WINAPI nD3D9Server_WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
