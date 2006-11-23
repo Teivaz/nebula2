fab7b94019a8a9b1c16ac3a3300528d600f938ba
kimsama <kimsama@a2aae4a0-4114-0410-8d4e-a30d9c0f8405>
2006-11-23 02:24:06 +0000

    Changed to filter shaders out explicitly with the "filter.xml" file. The plug-in does not generated script code for the shader which listed in the file. It provides convenient way to keep only necessary shaders on the material editor of 3dsmax.

--- nebula2/code/contrib/nmax/inc/export2/nmaxmaterial.h
+++ nebula2/code/contrib/nmax/inc/export2/nmaxmaterial.h
@@ -160,16 +160,4 @@ bool nMaxMaterial::IsPredefinedCA(TCHAR* caName)
 }
 
 //-----------------------------------------------------------------------------
-/**
-    Retrieve default texture filename from shader.xml.
-
-    @param shader shader alias which is alread assigned to the given node. i.g. "static", "skinned" etc.
-    @param param string of shader parameter such as 'nShaderState::DiffMap0' and so on. 
-    @param outvalue texture filename retrieved from database which will be used for default texture.
-
-    @return true, if the default value is found ohterwise false.
-*/
-bool GetDefaultValueFromDataBase(const nString &shader, const nString &param, nString &outvalue);
-
-//-----------------------------------------------------------------------------
 #endif
